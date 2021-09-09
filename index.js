2c26e5c562e679a2c0c82ccee22b5fea74c603bc
esmaben <felippecorrea2010@gmail.com>
2021-09-09 20:40:02 -0300

    Começando projeto: Criando arquitetura MSC

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// const MovieController = require('./controllers/movieController');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// app.post('/movies', MovieController.create);
+
+const PORT = process.env.PORT || 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
+}); 
