fca3f5c707b99671ed4d50d0c63c6aa022e46898
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-10 19:20:14 -0300

    criado validação de campos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,22 @@
+const rescue = require('express-rescue');
+const Joi = require('joi');
 const productsServer = require('../services/productsService');
 
-const create = async (req, res) => {
+const create = rescue(async (req, res, next) => {
   const { name, quantity } = req.body;
 
+  const { error } = Joi.object({
+    name: Joi.string().min(5).not().empty()
+      .required(),
+    quantity: Joi.number().min(1).not().empty(),
+  }).validate(req.body);
+
+  if (error) return next(error);
+
   const response = await productsServer.create({ name, quantity });
-  
+
   res.status(201).json(response);
-};
+});
 
 module.exports = {
   create,
