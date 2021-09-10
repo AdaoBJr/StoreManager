674527350860285ef7e1f982a9549ce614370463
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-09-10 16:21:53 -0300

    update: requisito 1

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,5 +1,3 @@
-const productsModel = require('../models/productsModel');
-
 const errors = {
   nameLength: '"name" length must be at least 5 characters long',
   productExists: 'Product already exists',
@@ -9,16 +7,9 @@ const errors = {
 
 const checkNameLength = (name, len) => name.length <= len;
 
-// const checkProductExists = async (name) => {
-//   const response = await productsModel.findByName(name);
-//   if (response) return true;
-//   return false;
-// };
-
 const checkIsNumber = (quantity) => typeof quantity !== 'number';
 
 const checkLessThanZ = (quantity) => quantity <= 0;
-// const checkIsInt = (quantity) => Number.isInteger(quantity);
 
 const isProductValid = (name, quantity) => {
 switch (true) {
@@ -29,26 +20,6 @@ switch (true) {
     return {};
 }
 };
-
-// const isNameValid = (name) => {
-//   if (
-//     !name
-//     || typeof name !== 'string'
-//     || name.length < 5
-//     // verificar se o name é unico
-//   ) return false;
-//   return true;
-// };
-
-// const isQuantityValid = (quantity) => {
-//   if (
-//     !quantity
-//     || typeof quantity !== 'number'
-//     || quantity <= 0
-//     || Number.isInteger(quantity) === false
-//   ) return false;
-//   return true;
-// };
 
 module.exports = {
   isProductValid,
