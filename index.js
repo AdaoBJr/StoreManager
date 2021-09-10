760c5778f5f843dce3378ca291b7d95b5788b4ec
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-10 17:39:17 -0300

    add bodyParser e comentarios sobre as partes do código

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const productsController = require('./controllers/productsController');
 
+// declarações
 const app = express();
 const PORT = 3000;
+
+// configs
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+// rotas
 app.get('/products', productsController.create);
+
 app.listen(PORT, () => console.log(`Servido rodando na porta: ${PORT}`));
