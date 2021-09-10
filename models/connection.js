36dceeec0600d9ba3d95b91fb9a10fb50d69e31e
Caio <thehaseol@gmail.com>
2021-09-10 18:44:09 -0300

    Fix connection

--- models/connection.js
+++ models/connection.js
@@ -1,20 +1,22 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
 const DB_NAME = 'StoreManager';
-let db;
 
-const connection = () =>
-  db
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+
+let db = null;
+
+const connection = () => (db
     ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-        .then((connection) => {
-          db = connection.db(DB_NAME);
-          return db;
-        })
-        .catch((err) => console.log(err));
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS).then((conn) => {
+        db = conn.db(DB_NAME);
+        return db;
+      }));
 
 module.exports = connection;
