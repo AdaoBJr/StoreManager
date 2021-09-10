dd9c549bf6d9e60ef19d4343cbc8f7bdc3e71684
André Arnoni <andre_arnoni@yahoo.com.br>
2021-09-10 14:55:02 -0300

    implementada a conexão com o banco(connection.js)

---
+++ models/connection.js
@@ -0,0 +1,30 @@
+const { MongoClient } = require('mongodb');
+
+// Conexão com o banco local:
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+// Conexão com o avaliador:
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
+
+let schema = null;
+
+async function getConnection() {
+  if (schema) return Promise.resolve(schema);
+  return MongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+module.exports = { getConnection };
