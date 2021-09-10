22ae8885cfd4257f4b0ccd38b209fa21e65f3f97
Letycia Barreto <letyciasbarreto@gmail.com>
2021-09-10 15:37:31 -0300

    connection

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = '3000';
+app.use(bodyParser).json();
+
+app.listen(PORT, () => {
+  console.log('Online');
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
---
+++ models/connection.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+let nulo = null;
+const connectionDb = () => (
+  nulo ? Promise.resolve(nulo) : MongoClient.connect(MONGO_DB_URL, { seNewUrlParser: true,
+    useUnifiedTopology: true }).then((conn) => {
+      nulo = conn.db(DB_NAME);
+      return nulo;
+    }));
+module.exports = connectionDb;
