a0e584797b32d6d8d69d839c18d4e72bf91cf0ac
Adão Benites <adaobjr@gmail.com>
2021-09-10 15:03:27 -0400

    feat: :tada: Starting Project

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const PORT = 3000;
+
+app.use((error, _req, res, _next) => {
+  res.status(error.status).json({ err: error.err });
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.listen(PORT, () => {
+  console.log('Rodando na porta:', PORT);
 });
