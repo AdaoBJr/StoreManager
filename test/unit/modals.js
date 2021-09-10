da9ea7cb375eb1d061522d46592eb60ac28810c1
Paulo Xavier <pxavierf@gmail.com>
2021-09-10 09:44:06 -0300

    criando conexão com o banco e criando estrutura de testes da rota /products

---
+++ test/unit/modals.js
@@ -0,0 +1,33 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const { MongoClient } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+const connection = require('../../models/connection');
+
+describe('Validações para a rota "/products"', () => {
+  describe('Listar todos os produtos', () => {
+
+  });
+
+  describe('Buscar um produto por ID', () => {
+
+  });
+
+  describe('Criar um produto', () => {
+
+  });
+
+  describe('Atualizar um produto', () => {
+
+  });
+
+  describe('Deletar um produto', () => {
+
+  });
+});
+
+describe('Validações para a rota "/sales"', () => {
+
+});
