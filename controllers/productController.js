55e5f63ebfd916c41c6fc95f018d26f0d94b1372
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-09-10 16:48:13 -0300

    Criado o arquivo productController

---
+++ controllers/productController.js
@@ -0,0 +1,3 @@
+const getAll = (req, res) => {};
+
+module.exports = { getAll };
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Iniciando o project store manager 
 // não remova esse endpoint, e para o avaliador funcionar
 const express = require('express');
+const bodyParser = require('body-parser');
+const productsController = require('./controllers/productController');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (_request, response) => {
   response.send();
