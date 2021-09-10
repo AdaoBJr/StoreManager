3c7207cebe66a14c71f2fa6ccf43a96cf40b4799
esmaben <felippecorrea2010@gmail.com>
2021-09-10 17:20:24 -0300

    DEV req2: Criando funçoes getAll e getbyid no models

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,4 +1,4 @@
-// const { ObjectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 const mongoConnection = require('./connection');
 
 const findByName = async (name) => {
@@ -13,7 +13,6 @@ const create = async ({ name, quantity }) => {
     .then((db) => db.collection('products'));
 
   const createdProduct = await productsCollection.insertOne({ name, quantity });
-  // const { insertedId: id } = createdProduct;
   return {
     id: createdProduct.insertedId,
     name,
@@ -21,17 +20,26 @@ const create = async ({ name, quantity }) => {
   };
 };
 
-// const getAll = async () => {
-//   const db = await mongoConnection.getConnection();
-//   const movies = await db.collection('movies')
-//     .find()
-//     .toArray();
+const getAll = async () => {
+  const db = await mongoConnection.getConnection();
+  const product = await db.collection('products')
+    .find()
+    .toArray();
 
-//   return movies
-// };
+  return product;
+};
+
+const getById = async (id) => {
+  if (!ObjectId.isValid(id)) return null;
+
+  const db = await mongoConnection.getConnection();
+  const productId = await db.collection('people').findOne(ObjectId(id));
+  return productId;
+};
 
 module.exports = {
   create,
   findByName,
-  /* getAll   */
+  getAll,
+  getById,
 };
