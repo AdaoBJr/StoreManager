12e16554a458b4b3d748802490ec220053384368
CIGR2021 <appencigr@gmail.com>
2021-09-10 17:20:36 -0300

    Estruturando MSC

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express');
+require('dotenv').config();
+require('module-alias/register');
 
-const app = express();
-
-app.use(express.json());
-
-const PORT = '3000';
+const config = require('@config');
+const app = require('@app');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.listen(PORT, () => {
-  console.log('Online');
+app.listen(config.app.port, (err) => {
+  if (err) {
+    return console.log('erro');
+  }
+  console.log('iniciou em http://localhost:3000');
 });
