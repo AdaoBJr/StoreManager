f211adb1cea2acbbc4e4f51dcf72068f5c30b83e
Paulo Xavier <pxavierf@gmail.com>
2021-09-10 18:24:37 -0300

    criando rotas  de /sales

--- router/routes.js
+++ router/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const ProductRouters = require('../controller/ProductController');
+const SalesRouter = require('../controller/SaleController');
 
 const router = express.Router();
 
 router.use('/products', ProductRouters);
+router.use('/sales', SalesRouter);
 
 module.exports = router;
