6b7cd691dfc847bb0fc4eec37d0b8df8c9753ba7
Emerson Saturnino <programador.emerson@hotmail.com>
2021-09-10 00:09:46 -0300

    Requisito 05

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const routes = require('./routes');
+const routesProducts = require('./routes/product.routes');
+const salesProducts = require('./routes/sales.routes');
 
 const app = express();
 app.use(express.json());
-app.use(routes);
+app.use(routesProducts);
+app.use(salesProducts);
 
 app.listen(3000, () => {
   console.log('Ouvindo a porta 3000');
