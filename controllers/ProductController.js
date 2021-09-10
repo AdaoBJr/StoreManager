7018f4ec7af22591b96447c777703e588bef4d37
RafaMaths <rmathscoach@gmail.com>
2021-09-10 12:37:26 -0300

    create getAll in Service

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,13 +1,17 @@
 const StatusCodes = require('http-status-codes');
 const ProductService = require('../services/ProductService');
 
+const getAll = async () => 
+
 const create = async (req, res) => {
- const { name, quantity } = req.body;
+const { name, quantity } = req.body;
  const product = await ProductService.create({ name, quantity });
+ 
+ const { id } = product;
  if (!product) {
   return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Dados inválidos' }); 
 }
- return res.status(StatusCodes.CREATED).json({ name, quantity });
+ return res.status(StatusCodes.CREATED).json({ id, name, quantity });
 };
 
 module.exports = { create };
--- services/ProductService.js
+++ services/ProductService.js
@@ -1,14 +1,54 @@
 const ProductModel = require('../models/ProductModel');
 
+// const getAll = async () => {
+//   return await ProductModel.getAll();
+// };
+
 const isValidName = (name) => {
   // name deve ser uma string com mais de 5 caracteres e deve ser único;
-  if (typeof name !== 'string' || name.length < 5) return false;
+  if (typeof name !== 'string') {
+    console.log(typeof name);
+    return false;/* {
+      err: {
+        code: 'invalid_data',
+        message: 'name must be string ',
+      },
+    }; */
+  }
+  
+  if (name.length < 5) {
+    return false;/* {
+      err: {
+        code: 'invalid_data',
+        message: 'name length must be at least 5 characters long',
+      },
+    }; */
+  }
+
+  // const product = ProductModel.findOne({ name });
+  // if (!product) {
+  //   return {
+  //     err: {
+  //       code: 'invalid_data',
+  //       message: 'Product already exists',
+  //     },
+  //   };
+  // }
   
   return true;
 };
 
 const isValidQuantity = (quantity) => {
-  if (typeof quantity !== 'number' && quantity <= 0) return false;
+  if (typeof quantity !== 'number') return false;
+ 
+  if (quantity < 0) {
+    return false; /* {
+      err: {
+        code: 'invalid_data',
+        message: 'quantity must be larger than or equal to 1',
+      },
+    }; */
+  }
 
   return true;
 };  
