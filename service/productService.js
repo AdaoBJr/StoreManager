590d58fce77f737cee1de6dbaac92ece87c8e19f
esmaben <felippecorrea2010@gmail.com>
2021-09-10 15:24:56 -0300

    DEV req1: Adicionando findbyname no service

--- service/productService.js
+++ service/productService.js
@@ -25,25 +25,22 @@ const isValidQuantityNotNumber = (quantity) => {
   }
   return true;
 };
-  
+
 const create = async (name, quantity) => {
   const isProductNameValid = isValidName(name);
   const isProductQuantityValidZero = isValidQuantityZero(quantity);
-  const isProductQuantityNotNumber = isValidQuantityNotNumber(quantity);  
+  const isProductQuantityNotNumber = isValidQuantityNotNumber(quantity);
+  const existingProduct = await productModel.findByName(name);
   if (!isProductNameValid) {
     return { code: 'invalid_data', message: '"name" length must be at least 5 caracters long' }; 
   }
   if (!isProductQuantityValidZero) {
     return { code: 'invalid_data', message: '"quantity" must be larger than or equal to 1' };
   }
-
   if (!isProductQuantityNotNumber) {
-    return {
-      code: 'invalid_data',
-      message: '"quantity" must be a number',
-    }; 
+    return { code: 'invalid_data', message: '"quantity" must be a number' }; 
   }
-
+  if (!existingProduct) { return { code: 'invalid_data', message: 'Product already exists' }; }
   const { id } = await productModel.create(name, quantity);
   return { id, name, quantity };
 };
