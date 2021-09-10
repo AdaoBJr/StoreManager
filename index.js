62953b5180fd218be00397bd94c37e269c3ff466
Rafael <rafaeljavaro750@gmail.com>
2021-09-10 15:00:35 -0300

    Configura express, App Online

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+require('dotenv/config');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => console.log(`App Online on Port:${PORT}`));
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
