3f255b42ea525e08e3fbc02ed65a53f8ce59fd51
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-09-10 15:42:39 -0300

    Estrutura inicial do projeto: pastas MSC e middleware

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+
+const errorMiddleware = require('./middlewares/error');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(errorMiddleware);
+
+const LOCALHOST = 3000;
+const port = process.env.PORT || LOCALHOST;
+
+app.listen(port, () => { console.log(`Ouvindo na porta ${port}`); });
