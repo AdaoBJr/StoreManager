da5f7df6b13e32fdd26919c6bee46ba1a4ee294e
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-10 17:30:30 -0300

    criado função que add dados ao banco

--- models/productsModels.js
+++ models/productsModels.js
@@ -1,5 +1,17 @@
-const create = () => {
+const mongoConnection = require('./connection');
 
+const create = async ({ name, quantity }) => {
+  const productsCollection = await mongoConnection.getConnection()
+    .then((db) => db.collection('products'));
+
+  const { insertedId: id } = productsCollection
+    .insertOne({ name, quantity });
+
+  return {
+    id,
+    name,
+    quantity,
+  };
 };
 
 module.exports = { create };
