7e759f28d4f655d08f37c41cff1608dba6585ffa
Mariana Mohr <marianamohr@gmail.com>
2021-09-10 14:41:30 -0300

    Inicio do projeto

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const productsRouter = require('./routes/productsRouter');
+
+const PORT = 3000;
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.json({ message: 'Funcionando !!!' }));
+app.use('/products', productsRouter);
+
 // não remova esse endpoint, e para o avaliador funcionar
+
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => { console.log(`Api rodando na porta ${3000}`); });
