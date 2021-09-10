b246029f234482b1013561f4653f456b8be8fc28
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-09-10 12:08:17 -0300

    create index od app

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+// rodas dos recursos aqui
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT} | http://localhost/${PORT},`));
