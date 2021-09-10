cff0dccca8c95665ea131cb54d0820fca13b5e38
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-09-10 11:43:24 -0300

    added skips to test files

---
+++ models/connection.js
@@ -0,0 +1,7 @@
+// conexão local
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
+
+// conexão avaliador
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
