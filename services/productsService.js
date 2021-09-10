a90f72fbf6179d0a19f95b3546dd577daa14aca4
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-10 17:38:01 -0300

    criado função create no service

--- services/productsService.js
+++ services/productsService.js
@@ -1,5 +1,8 @@
-const create = () => {
+const productsModel = require('../models/productsModels');
 
+const create = async ({ name, quantity }) => {
+  const result = await productsModel.create({ name, quantity });
+  return result;
 };
 
 module.exports = { create };
