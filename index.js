18008e73f618d63f85b83509639f9c942c3bf169
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-09-10 15:30:19 -0300

    Consertado o erro com o app

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // Iniciando o project store manager 
 // não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+
+const app = express();
+
 app.get('/', (_request, response) => {
   response.send();
 });
