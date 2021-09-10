aa8fbdb175a9acfee6b713a8a26c3d526abe3987
Rafael <rafaeljavaro750@gmail.com>
2021-09-10 19:43:43 -0300

    Req 5 - Cria EndPoint que adiciona nova venda - Valida o Input

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const ckeckNewProductInput = require('./checkNewProductInput');
 const checkIfNameInputIsTaken = require('./checkIfNameInputIstaken');
 const checkiDInputed = require('./checkiDInputed');
+const checkNewSalesInput = require('./checkNewSalesInput');
 
 module.exports = {
     ckeckNewProductInput,
     checkIfNameInputIsTaken,
     checkiDInputed,
+    checkNewSalesInput,
 };
--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const salesController = require('../controller/salesController');
+const { checkNewSalesInput } = require('../middlewares');
 
 const router = express.Router();
 
-router.post('/', salesController.createSales);
+router.post('/', checkNewSalesInput, salesController.createSales);
 
 module.exports = router;
