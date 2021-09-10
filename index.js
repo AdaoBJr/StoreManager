7bdaa2081d2c6c2bab68bdc922446e55dd887863
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-10 17:16:07 -0300

    configuração inicial

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const productsController = require('./controllers/productsController');
+
+const app = express();
+const PORT = 3000;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/products', productsController.create);
+app.listen(PORT, () => console.log(`Servido rodando na porta: ${PORT}`));
