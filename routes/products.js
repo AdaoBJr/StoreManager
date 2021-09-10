ebd8c4eda553aa84f3e714df03be28c22384f3ae
Caio <thehaseol@gmail.com>
2021-09-10 16:55:11 -0300

    Fix and create some more Endpoints

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const products = require('../controllers/products');
 const validate = require('../middlewares/validator');
@@ -8,5 +7,7 @@ const route = express.Router();
 route.post('/', validate.product, validate.productExists, products.create);
 route.get('/', products.getAll);
 route.get('/:id', validate.productId, products.getById);
+route.put('/:id', validate.product, products.update);
+route.delete('/:id', validate.productId, products.remove);
 
 module.exports = route;
