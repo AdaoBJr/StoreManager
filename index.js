7b3f85399b00bd5cf2a80bbe343332038fda1bcf
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-09-10 13:47:58 -0300

    config index.js & connection with mongo db

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
+app.get('/', (_req, res) => {
+  res.send();
 });
+
+app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
---
+++ models/connection.js
@@ -0,0 +1,20 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+const connection = () =>
+  MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.log(err);
+      process.exit();
+    });
+
+module.exports = connection;
+
+// products : { "name": "Produto Silva", "quantity": 10 }
+// sales : { "itensSold": [{ "productId": "5f43cbf4c45ff5104986e81d", "quantity": 2 }] }
