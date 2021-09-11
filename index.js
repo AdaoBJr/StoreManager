a193c1708d0938d172a24acf5993253e4da27b56
denis-rossati <denis.rossati@hotmail.com>
2021-09-11 03:59:54 -0300

    correting lint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const productsRouter = require('./routes/products');
-const salesRouter = require('./routes/sales');
+const salesRouter = require('./routes/sales.js');
 
 const app = express();
 app.use(bodyParser.json());
