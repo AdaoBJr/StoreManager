197d723908e7a6e9ff7b32b0ee21e378ebc6e1c6
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-09-11 08:43:30 -0300

    Test CI environment

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+if (process.env.CI === 'true') {
+  console.log('Running in CI mode');
+}
