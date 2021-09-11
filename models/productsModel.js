d93faa778d7e7721b718c4cbe898aab96fc9ead8
Lara Capila <laracapilak@gmail.com>
2021-09-11 15:42:44 -0300

    Implementa busca no banco para getAll e getById

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,3 +1,5 @@
+const { ObjectId } = require('mongodb');
+
 const connection = require('./mongoConnetion');
 
 const findProductByName = async (name) => {
@@ -10,13 +12,29 @@ const findProductByName = async (name) => {
 };
 
 const createProductModel = async ({ name, quantity }) => {
-  const db = connection();
+  const db = await connection();
   const createResult = await db.collection('porducts').insertOne({ name, quantity });
 
   return { _id: createResult.insertedId, name, quantity };
 };
 
+const getAll = async () => {
+  const db = await connection();
+  const getAllProducts = await db.collection('products').find().toArray();
+  return getAllProducts;
+};
+
+const getProductById = async (id) => {
+  const db = await connection();
+  const product = await db.collection('products').findOne(ObjectId(id));
+
+  if (!product) return null;
+  return product;
+};
+
 module.exports = {
   findProductByName,
   createProductModel,
+  getAll,
+  getProductById,
 };
