2f8964f61e2c1c27198524e0b1df0c07f37ebc94
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-10 21:10:59 -0300

    add class que trata erros personalizados

---
+++ helpers/InternalServerError.js
@@ -0,0 +1,10 @@
+class InternarServerError extends Error {
+  constructor(code, msg, statusCode) {
+    super(code, msg, statusCode);
+    this.code = code;
+    this.message = msg;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = InternarServerError;
--- middlewares/error.js
+++ middlewares/error.js
@@ -1,7 +1,13 @@
-module.exports = (err, req, res, _next) => {
+module.exports = (err, _req, res, _next) => {
   if (err.isJoi) {
     return res
       .status(422)
       .json({ err: { code: 'invalid_data', message: err.details[0].message } });
   }
+
+  if (err.statusCode) {
+    const { code, message, statusCode } = err;
+    return res.status(statusCode).json({ err: { code, message } });
+  } 
+    return res.status(500).json({ message: 'Matrix Error!' });
 };
