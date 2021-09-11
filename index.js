01637867f4f94e69aa5f40896ec5ae45667722eb
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-09-11 15:52:16 -0300

    requisito 10

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
 
 app.use(routesProducts);
 app.use(salesProducts);
