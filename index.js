819b6aa60c38e60709c05079b9ac54ad47191805
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-11 09:36:05 -0300

    Creating MSC Layers and Basic Setting Index

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// não remova esse endpoint abaixo, ele está ai para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
