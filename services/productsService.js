bd873095442afee1ba64e66527803826af1a0c8a
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-10 21:16:56 -0300

    integração da class  de erro personalizado ao productService.

--- services/productsService.js
+++ services/productsService.js
@@ -1,8 +1,17 @@
 const productsModel = require('../models/productsModels');
+const InternarServerError = require('../helpers/InternalServerError');
 
 const create = async ({ name, quantity }) => {
+  const product = await productsModel.findName({ name });
+  if (product) {
+    throw new InternarServerError('invalid_data', 'Product already exists', 422);
+  }
+
   const response = await productsModel.create({ name, quantity });
   return response;
 };
 
 module.exports = { create };
+
+// peguei a dica de como criar erro personalizando desse canal:
+// https://www.youtube.com/watch?v=qHfZxpRqxYw
