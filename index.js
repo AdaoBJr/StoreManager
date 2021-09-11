9e98a69b52a6399c5a4e30d4ee71e2c495edafcc
Abimaelr <abimaelr.albu@gmail.com>
2021-09-11 12:24:42 -0300

    Questão 1 quase finalizada

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+const products = require('./routes/products');
+
+const app = express();
+
+app.use(express.json());
+app.use('/products', products);
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(3000, () => console.log('Estou na porta 3000'));
