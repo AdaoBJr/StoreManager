ba226a310610c7178a29aabb782d5f31a4be8e1d
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-12 11:19:37 -0300

    consertando strutura do middleware de error.

--- middlewares/error.js
+++ middlewares/error.js
@@ -2,10 +2,11 @@ module.exports = (err, _req, res, _next) => {
   if (err.statusCode) {
     const { code, message, statusCode } = err;
     return res.status(statusCode).json({ err: { code, message } });
-  } 
-    return res.status(500).json({
-      err: { 
-        code: 'Internal_Server_Error', message: err.message, 
-      },
-    });
+  }
+  
+  return res.status(500).json({
+    err: { 
+      code: 'Internal_Server_Error', message: err.message, 
+    },
+  });
 };
