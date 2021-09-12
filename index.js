1529e318274325cc520efd2331b81a0cdc1c1f39
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-11 21:39:17 -0300

    Inserting validation middlewares on index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const validationMiddlewares = require('./src/Middlewares/validationMiddlewares');
 const productsRouter = require('./src/routers/productsRouter');
+const errorMiddleware = require('./src/Middlewares/errorMiddleware');
 // const salesRouter = require('./src/routers/salesRouter');
 
 const app = express();
@@ -12,8 +14,14 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use('/products', productsRouter);
+app.use('/products',
+validationMiddlewares.validateNameLength,
+validationMiddlewares.validateQuantityType,
+validationMiddlewares.validateQuantityQuantity,
+productsRouter);
 
 // app.use('/sales', salesRouter);
+
+app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
