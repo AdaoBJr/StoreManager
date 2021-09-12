6c7ea41a02e56cd6cfb5c1b99103687fa1947537
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-12 10:34:55 -0300

    padronizando middleware de error

--- middlewares/error.js
+++ middlewares/error.js
@@ -9,5 +9,9 @@ module.exports = (err, _req, res, _next) => {
     const { code, message, statusCode } = err;
     return res.status(statusCode).json({ err: { code, message } });
   } 
-    return res.status(500).json({ code: 'Internal_Server_Error', message: err.message });
+    return res.status(500).json({
+      err: { 
+        code: 'Internal_Server_Error', message: err.message, 
+      },
+    });
 };
