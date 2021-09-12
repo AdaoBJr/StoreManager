aa128e8fd5f729d7aa99f865fe4fad925ee21679
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-11 21:40:09 -0300

    Inserting on products service some verification

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -1,6 +1,13 @@
 const productsModel = require('../models/productsModel');
 
-const registerProduct = async (name, quantity) =>
-  productsModel.registerProduct(name, quantity);
+const registerProduct = async (name, quantity) => {
+  const result = await productsModel.checkName(name);
+  if (result.length > 0) {
+    return {
+      err: { message: 'Product already exists', code: 'invalid_data', status: 422 },
+    };
+  }
+  return productsModel.registerProduct(name, quantity);
+};
 
 module.exports = { registerProduct };
