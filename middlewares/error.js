037b4ab5a8da9281c9a4898d58e2e38d86bd6a3f
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-12 11:17:27 -0300

    removido condicional para saber se o error é do joi

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,10 +1,4 @@
 module.exports = (err, _req, res, _next) => {
-  if (err.isJoi) {
-    return res
-      .status(422)
-      .json({ err: { code: 'invalid_data', message: err.details[0].message } });
-  }
-
   if (err.statusCode) {
     const { code, message, statusCode } = err;
     return res.status(statusCode).json({ err: { code, message } });
