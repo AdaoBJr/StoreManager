40ebe650007f268965e2ef792c45b7c92d119e4e
tonpasso <wellpasso@gmail.com>
2021-09-12 09:37:51 -0300

    iniciando projeto

---
+++ models/mongoConnection.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// LOCAL
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+
+// GIT
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+
+let db = null;
+
+const connection = () => (db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db('model_example');
+      return db;
+    }));
+
+module.exports = connection;
