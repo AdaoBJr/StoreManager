1130f264f05dc170aab5ab9b655720662ef3db31
Diegho <dieghonm@gmail.com>
2021-09-12 16:30:48 -0300

    Requisito 1

--- 3services/product_services.js
+++ 3services/product_services.js
@@ -7,7 +7,7 @@ const validaNumber = (quantity) => (typeof quantity !== 'number');
 
 // fiz com a ajuda do Renato
 // https://github.com/tryber/sd-010-b-store-manager/pull/43/files
-const UnprocessableEntity = 422;
+const code = 422;
 
 const validastring = '"name" length must be at least 5 characters long';
 
@@ -17,16 +17,18 @@ const valueQuantityNotValid = '"quantity" must be larger than or equal to 1';
 
 const typeQuantityNotValid = '"quantity" must be a number';
 
+const error = (message) => ({ err: { code, message } });
+
 const createproducts = async (name, quantity) => {
   switch (true) {
     case validaName(name):
-      return { err: { validastring, UnprocessableEntity } };
+      return error(validastring);
     case await existItem(name):
-      return { err: { productExist, UnprocessableEntity } }; 
+      return error(productExist);
     case validaQuantity(quantity):
-      return { err: { valueQuantityNotValid, UnprocessableEntity } };
+      return error(valueQuantityNotValid);
     case validaNumber(quantity):
-      return { err: { typeQuantityNotValid, UnprocessableEntity } };   
+      return error(typeQuantityNotValid);   
     default:
       return create(name, quantity);
   }
