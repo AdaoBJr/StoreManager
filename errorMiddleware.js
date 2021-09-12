2316bedfc8cc2d55af1829fcd11792ba4d63e9f5
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-12 16:40:11 -0300

    Leaving only error middleware

---
+++ errorMiddleware.js
@@ -0,0 +1,13 @@
+const { dictionary } = require('./src/services/productsService');
+
+const errorMiddleware = (err, _req, res, _next) => {
+  const { internalServerError } = dictionary().status;
+
+  if (err.status && err.code) {
+    return res.status(err.status).json({ err: { code: err.code, message: err.message } });
+  }
+
+  return res.status(internalServerError).json({ message: err.message });
+};
+
+module.exports = { errorMiddleware };
