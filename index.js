a1c2157f981399f3fe0249461af951d75acb1a14
Flavio Andrade <foa12381@gmail.com>
2021-09-12 09:06:54 -0300

    config routes env

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const products = require('./routers/ProductsRouter');
+const sales = require('./routers/SalesRouter');
+
+require('dotenv').config();
 
 const app = express();
+const URL_PORT = 3000;
+const PORT = process.env.PORT || URL_PORT;
+
+app.use(bodyParser.json());
+
+app.use('/products', products);
+
+app.use('/sales', sales);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Oline porta ${PORT}`));
---
+++ routers/SalesRouter.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const SalesRouter = express.Router();
+
+module.exports = SalesRouter;
