cdbbfd859ef2548f541322cfb3476ea25b735ed1
dennisMarcelo <dennismarcelo15@gmail.com>
2021-09-13 10:37:06 -0300

    Revert "Estrutura inicial da rota de criação da rota sales"
    This reverts commit 9d30906aa4ae343a029b34e2063a4d149f6095e7.

--- services/salesService.js
+++
@@ -1,9 +0,0 @@
-const salesModel = require('../models/salesModel');
-
-const create = async (itensSold) => {
-  const id = await salesModel.create(itensSold);
-
-  return id;
-};
-
-module.exports = { create };
