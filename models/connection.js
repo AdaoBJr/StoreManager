204fcb3ed81d033a68ea5b54359abf27ecce017f
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-09-13 17:31:17 -0300

    fix connection

--- models/connection.js
+++ models/connection.js
@@ -6,9 +6,9 @@ const OPTIONS = {
 };
 
 // url para o avaloador
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 let db = null;
