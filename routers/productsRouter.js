a7f2b20c7ca967a6766072850694cad64f0f5c0d
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-13 17:39:50 -0300

    Feat: Cria a rota para o update

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -2,16 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-const { create, getAll, getById } = require('../controllers/productsController');
+const { create, getAll, getById, update } = require('../controllers/productsController');
 const {
   validName,
   productExists,
   validQuantity,
   existsId,
+
   } = require('../middlewares/middlewaresProducts');
 
 router.get('/products', getAll);
 router.get('/products/:id', existsId, getById);
 router.post('/products', validName, validQuantity, productExists, create);
+router.put('/products/:id', validName, validQuantity, update);
 
 module.exports = router;
