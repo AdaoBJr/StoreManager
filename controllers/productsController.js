b4ea8e6b11bd975cd55de6b7007d9152e2073ada
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-13 17:01:21 -0300

    Feat: Chamada de todos os produtos e chamada com id

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -4,19 +4,23 @@ const service = require('../services/productsService');
 
 const getAll = rescue(async (_request, response) => {
   const products = await service.getAll();
+  response.status(StatusCodes.OK).json({ products });
+});
 
-  response.status(200).json(products);
+const getById = rescue(async (request, response) => {
+  const { id } = request.params;
+  const product = await service.findId(id);
+  response.status(StatusCodes.OK).json(product);
 });
 
 const create = rescue(async (request, response) => {
   const { name, quantity } = request.body;
-
   const newProduct = await service.createProduct({ name, quantity });
-
   return response.status(StatusCodes.CREATED).json(newProduct);
 });
 
 module.exports = {
   create,
   getAll,
+  getById,
 };
