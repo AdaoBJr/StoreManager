1ff5e47904ee2d63d0188791beb18d264878f8f8
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-13 16:00:40 -0300

    Refactor: Muda o nome do banco para avaliação

--- models/connection.js
+++ models/connection.js
@@ -1,13 +1,12 @@
 const { MongoClient } = require('mongodb');
-require('dotenv').config();
 
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL
-|| 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// 'mongodb://localhost:27017/StoreManager';
 
 const connection = () => MongoClient
   .connect(MONGO_DB_URL, OPTIONS)
