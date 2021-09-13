f30a368edaeb5ea2ea286e783dbe4bc914ed0961
RafaMaths <rmathscoach@gmail.com>
2021-09-13 15:30:49 -0300

    getAllSale

--- models/SaleModel.js
+++ models/SaleModel.js
@@ -6,22 +6,27 @@ const createSale = async ({ productId, quantity }) => {
   const salesCollection = await Connection.getConnection()
     .then((db) => db.collection('sales'));
 
-    // const productID = await ProductModel.findProductById(productId);
-        
     const { insertedId: id } = await salesCollection.insertOne({
       productId,
       quantity,
     });
-    // console.log(productId, 'ProductId');
-    // console.log(quantity, 'Quantidade');
-            
+           
     return {
       id,
       productId,
       quantity,
     };
   };
-  
+
+  const getAllSale = async () => {
+    const salesCollection = await Connection.getConnection()
+      .then((db) => db.collection('sales'));
+   
+    const sales = await salesCollection.find({}).toArray();
+    return sales;
+  };
+    
 module.exports = {
   createSale,
+  getAllSale,
 };
