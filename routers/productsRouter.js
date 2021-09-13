2502763c12e685094ab9aaab5c729b94e65aba20
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-13 17:03:13 -0300

    Feat: Cria a rota para busca pelo id

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
-const { create, getAll } = require('../controllers/productsController');
+const { create, getAll, getById } = require('../controllers/productsController');
 const {
   validName,
   productExists,
   validQuantity,
+  existsId,
   } = require('../middlewares/middlewaresProducts');
 
 router.get('/products', getAll);
+router.get('/products/:id', existsId, getById);
 router.post('/products', validName, validQuantity, productExists, create);
 
 module.exports = router;
