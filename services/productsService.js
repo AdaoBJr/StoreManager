29b07635ab1c84b99c053ec9acd85f85fe89905c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-13 17:04:13 -0300

    Feat: Faz a chamada de busca para o model

--- services/productsService.js
+++ services/productsService.js
@@ -1,13 +1,22 @@
 const products = require('../models/products');
 
-const getAll = async () => products.getAll();
+const getAll = async () => {
+  const productsAll = products.getAll();
+  return productsAll;
+};
 
 const createProduct = async (product) => {
   const newProduct = products.create(product);
   return newProduct;
 };
 
+const findId = async (id) => {
+  const product = await products.findId(id);
+  return product;
+};
+
 module.exports = {
   createProduct,
   getAll,
+  findId,
 };
