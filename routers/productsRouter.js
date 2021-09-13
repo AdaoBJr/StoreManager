37575e03209ff4f4a7d92a922394d698b14dbd45
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-13 18:30:46 -0300

    Feat: Rota que deleta produto

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -2,18 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
-const { create, getAll, getById, update } = require('../controllers/productsController');
+const {
+  create,
+  getAll,
+  getById,
+  update,
+  deleteProduct,
+} = require('../controllers/productsController');
+
 const {
   validName,
   productExists,
   validQuantity,
   existsId,
-
   } = require('../middlewares/middlewaresProducts');
 
 router.get('/products', getAll);
 router.get('/products/:id', existsId, getById);
 router.post('/products', validName, validQuantity, productExists, create);
 router.put('/products/:id', validName, validQuantity, update);
+router.delete('/products/:id', existsId, deleteProduct);
 
 module.exports = router;
