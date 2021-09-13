fb60d5f1b5341aac6530adc2b491cfb9c4171470
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-13 17:02:03 -0300

    Feat: Verifica se o id para a busca do produdo é valido

--- middlewares/middlewaresProducts.js
+++ middlewares/middlewaresProducts.js
@@ -56,8 +56,24 @@ const validQuantity = async (request, response, next) => {
   next();
 };
 
+const existsId = async (request, response, next) => {
+  const { id } = request.params;
+  const productId = await productsModel.findId(id);
+
+  if (!productId) {
+    return response.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      err: {
+        code: 'invalid_data',
+        message: 'Wrong id format',
+      },
+    });
+  }
+  next();
+};
+
 module.exports = {
   productExists,
   validName,
   validQuantity,
+  existsId,
 };
