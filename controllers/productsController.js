d5fbf647fbc5518cffbc9a75c66dfe67ce331f90
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-13 18:29:32 -0300

    Feat: Controller de requisição para deletar produto

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -27,9 +27,16 @@ const update = rescue(async (request, response) => {
   response.status(StatusCodes.OK).json(product);
 });
 
+const deleteProduct = rescue(async (request, response) => {
+  const { id } = request.params;
+  const product = await service.deleteProduct(id);
+  return response.status(StatusCodes.OK).json(product);
+});
+
 module.exports = {
   create,
   getAll,
   getById,
   update,
+  deleteProduct,
 };
