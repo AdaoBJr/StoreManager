1c859ff0d4df87a05b7ff838aee321f6fae71947
Luiz Reina <luizreeina@gmail.com>
2021-09-14 14:32:10 -0300

    fixed name of collection products

--- models/connection.js
+++ models/connection.js
@@ -4,6 +4,7 @@ let connection = null;
 
 const DB_NAME = 'StoreManager';
 const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const LOCAL_MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 const getConnection = async () => {
   connection = connection || await mongoClient.connect(MONGO_DB_URL, {
--- models/productsModel.js
+++ models/productsModel.js
@@ -2,13 +2,13 @@ const connection = require('./connection');
 
 const createProduct = async ({ name, quantity }) => {
   const result = await connection.getConnection().then((db) =>
-    db.collection('produtos').insertOne({ name, quantity }));
+    db.collection('products').insertOne({ name, quantity }));
   return { id: result.insertedId, name, quantity };
 };
 
 const findProductByName = async (name) => {
   connection.getConnection().then((db) =>
-    db.collection('produtos').find({ name }));
+    db.collection('products').find({ name }));
 };
 
 module.exports = { createProduct, findProductByName };
