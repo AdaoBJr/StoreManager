3b4cdd8fa25db37a21d3b63acd7dd25d5f14065d
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-14 06:30:28 -0300

    feat: :tada:

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => 
+  console.log(`Server listening on port ${PORT}`));
