53e6a667e972825b33fe6c9982878a3374a54c79
Luiz Reina <luizreeina@gmail.com>
2021-09-14 12:58:21 -0300

    evaluators crash continues to happen

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const productsRouter = require('./controllers/productsController');
-const salesRouter = require('./controllers/salesController');
+// const productsRouter = require('./controllers/productsController');
+// const salesRouter = require('./controllers/salesController');
 
 const PORT = '3000';
 
@@ -14,8 +14,8 @@ app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use('/products', productsRouter);
-app.use('/sales', salesRouter);
+// app.use('/products', productsRouter);
+// app.use('/sales', salesRouter);
 
 app.listen(PORT, () => {
   console.log('Now live running at PORT 3000');
--- models/connection.js
+++ models/connection.js
@@ -3,11 +3,12 @@ const { mongoClient } = require('mongodb');
 let connection = null;
 
 const DB_NAME = 'StoreManager';
-const MONGO_DB_URL = `mongodb://mongodb:27017/${DB_NAME}`;
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const getConnection = async () => {
   connection = connection || await mongoClient.connect(MONGO_DB_URL, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
     .then((conn) => conn.db(DB_NAME));
   return connection;
