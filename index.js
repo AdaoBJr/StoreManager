ba6246bdfdc93d3b86341482e42b72c08052a87e
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-14 20:56:12 -0300

    feat: :sparkles: use productRouter and express.json in index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const { productRouter } = require('./controllers/productController');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productRouter);
 
 app.listen(PORT, () => 
   console.log(`Server listening on port ${PORT}`));
