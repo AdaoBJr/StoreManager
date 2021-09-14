92d2921b26a5c66b0e97d4a2debb0b394ec15fc2
RafaMaths <rmathscoach@gmail.com>
2021-09-13 21:29:20 -0300

    create getSaleById in model

--- models/SaleModel.js
+++ models/SaleModel.js
@@ -1,4 +1,4 @@
-// const { ObjectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 const Connection = require('./connection');
 // const ProductModel = require('./ProductModel');
 
@@ -17,10 +17,22 @@ const createSale = async (body) => {
   };
 
   const getAllProducts = async () => {
-    const salesCollection = await Connection.getConnection()
+    const salesCollection = await Connection.getConnection();
     const sales = await salesCollection('sales').find().toArray();
     return sales;
+  };
+
+  const getSaleById = async (id) => {
+    if (!ObjectId.isValid(id)) {
+      return null;
+    }
+    const salesCollection = await Connection.getConnection();
+    const sale = await salesCollection('sales').findOne({ _id: ObjectId(id) });
+    return sale;
+  };
 
 module.exports = {
   createSale,
+  getAllProducts,
+  getSaleById,
   };
