c44685ee9156c053da8248095c66dd76dec01feb
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-09-14 15:15:48 -0300

    required express/etc in index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
