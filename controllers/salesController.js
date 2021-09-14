c22d8e0a70a63f968c36af8d84b65111c23db89a
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-14 09:34:37 -0300

    Feat: Controller update

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -16,11 +16,18 @@ const create = rescue(async (request, response) => {
 const getById = rescue(async (request, response) => {
   const { id } = request.params;
   const product = await Service.findId(id);
-  response.status(StatusCodes.OK).json(product);
+  return response.status(StatusCodes.OK).json(product);
+});
+
+const update = rescue(async (request, response) => {
+  const { id } = request.params;
+  const sales = await Service.update(id, request.body);
+  return response.status(StatusCodes.OK).json(sales);
 });
 
 module.exports = {
   create,
   getAll,
   getById,
+  update,
 };
