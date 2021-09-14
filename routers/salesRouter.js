0e67379d64a527289ec4b0d393f37360585261c7
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-14 10:26:22 -0300

    Feat: Cria a rota para a exclusão de venda

--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const { create, getAll, update } = require('../controllers/salesController');
-const { findId, validQuantity } = require('../middlewares/middlewaresSales');
+const { create, getAll, update, deleteProduct } = require('../controllers/salesController');
+const { findId, validQuantity, validId } = require('../middlewares/middlewaresSales');
 
 router.get('/sales', getAll);
 router.get('/sales/:id', findId, getAll);
 router.post('/sales', create);
 router.put('/sales/:id', validQuantity, update);
+router.delete('/sales/:id', validId, deleteProduct);
 
 module.exports = router;
