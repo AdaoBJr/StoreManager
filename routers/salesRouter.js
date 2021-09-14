f4bda553772818ef2a6e5d47b87f23ee385014e6
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-14 09:36:22 -0300

    Feat: Cria a rota para update de vendas

--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -2,11 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-const { create, getAll } = require('../controllers/salesController');
-const { findId } = require('../middlewares/middlewaresSales');
+const { create, getAll, update } = require('../controllers/salesController');
+const { findId, validQuantity } = require('../middlewares/middlewaresSales');
 
 router.get('/sales', getAll);
 router.get('/sales/:id', findId, getAll);
 router.post('/sales', create);
+router.put('/sales/:id', validQuantity, update);
 
 module.exports = router;
