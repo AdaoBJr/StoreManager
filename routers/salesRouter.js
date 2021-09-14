ba7c69d555fe1e8ba524a3b7c597e13237ff799e
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-09-14 15:08:15 -0300

    created functions of get sales by Id

--- routers/salesRouter.js
+++ routers/salesRouter.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const salesController = require('../controllers/salesController');
 
 router.post('/', salesController.createSale);
+router.get('/:id', salesController.getSalesById);
+router.get('/', salesController.getAll);
+
 // router.post('/', productController.createProduct);
 module.exports = router;
