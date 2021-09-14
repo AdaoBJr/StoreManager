4770b4eab8c2cd2ce35cfcbae435c8d85c4e0541
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-09-14 08:32:25 -0300

    Feat: Querys para busca pelo id/lista de todas as vendas/create

---
+++ models/sales.js
@@ -0,0 +1,30 @@
+const { ObjectId } = require('mongodb');
+const connection = require('./connection');
+
+const getAll = async () => {
+  const responseConnection = await connection();
+  return responseConnection.collection('sales').find().toArray();
+};
+
+const findId = async (id) => {
+  if (!ObjectId.isValid(id)) {
+    return null;
+  }
+  const responseConnection = await connection();
+  return responseConnection.collection('sales').findOne(ObjectId(id));
+};
+
+const create = async (itensSold) => {
+  const responseConnection = await connection();
+  const { insertedId } = await responseConnection.collection('sales').insertOne({ itensSold });
+  return {
+    _id: insertedId,
+    itensSold,
+  };
+};
+
+module.exports = {
+  getAll,
+  create,
+  findId,
+};
