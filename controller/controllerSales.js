cdcbfacea265e4189176b9d1099a4f45e545803e
isTorres <ismaeltorres612@gmail.com>
2021-09-14 19:00:30 -0300

    build create method (controller)

--- controller/controllerSales.js
+++ controller/controllerSales.js
@@ -2,11 +2,19 @@ const ServiceSales = require('../service/serviceSales');
 
 const create = async (req, res) => {
   const itensSold = req.body;
-
   const salesMade = await ServiceSales.create(itensSold);
+  
+  if (salesMade.err) return res.status(422).json(salesMade);
+  if (salesMade.outStock) {
+    return res.status(422).json({ 
+      err: { 
+        code: 'invalid_data',
+        message: 'Wrong product ID or invalid quantity',
+      },
+    }); 
+  }
 
-  if (!salesMade) return res.status(422).json({ message: 'erro' });
-  return salesMade;
+  return res.status(200).json(salesMade);
 };
 
 module.exports = { create };
