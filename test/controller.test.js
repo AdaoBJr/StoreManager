e79035ab37c0219696e3edd28fab121bf1b8ede1
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-09-14 22:22:34 -0300

    Restore bug test fixed

--- test/controller.test.js
+++ test/controller.test.js
@@ -1,76 +1,75 @@
 const sinon = require("sinon");
 const { expect } = require("chai");
 const BaseController = require("../controllers/Base");
-const ProductService = require('../services/ProductService');
-const {ProductsController, SalesController} = require("../controllers");
+const ProductService = require("../services/ProductService");
+const { ProductsController, SalesController } = require("../controllers");
 
+const productService = new ProductService();
 describe("testa a BaseController", () => {
   let baseController;
   const response = {};
   const request = {};
 
   beforeAll(() => {
-
     request.body = {};
 
-    response.status = sinon.stub()
-      .returns(response);
-    response.json = sinon.stub()
-      .returns();
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns();
 
-    const findAllRes = { status: 200, message: { products: 'sou um teste' } }
-    const baseC = new BaseController(new ProductService())
-    sinon.stub(baseC.service, "FindAll").callsFake(() => findAllRes)
-    sinon.stub(baseC.service, "FindById").callsFake(() => findAllRes)
-    sinon.stub(baseC.service, "Delete").callsFake(() => findAllRes)
-    sinon.stub(baseC.service, "InsertOne").callsFake(() => findAllRes)
-    baseController = baseC
-  })
+    const findAllRes = { status: 200, message: { products: "sou um teste" } };
+    const baseC = new BaseController(productService);
+    sinon.stub(baseC.service, "FindAll").callsFake(() => findAllRes);
+    sinon.stub(baseC.service, "FindById").callsFake(() => findAllRes);
+    sinon.stub(baseC.service, "Delete").callsFake(() => findAllRes);
+    sinon.stub(baseC.service, "InsertOne").callsFake(() => findAllRes);
+    baseController = baseC;
+  });
 
   afterAll(() => {
     baseC.service.FindAll.restore;
     baseC.service.FindById.restore;
     baseC.service.Delete.restore;
     baseC.service.RootInsert.restore;
-  })
+  });
 
   it("testa o retorno do FindAll", async () => {
-    await baseController.RootFindAll(request, response)
-    expect(response.status.calledWith(200)).to.be.equal(true)
-    expect(response.json.calledWith({products: 'sou um teste'}))
-      .to.be.equal(true)
-  })
+    await baseController.RootFindAll(request, response);
+    expect(response.status.calledWith(200)).to.be.equal(true);
+    expect(response.json.calledWith({ products: "sou um teste" })).to.be.equal(
+      true
+    );
+  });
 
   describe("quando passado um id", () => {
-    
     beforeAll(() => {
-      request.params = {id: '300'}
-    })
+      request.params = { id: "300" };
+    });
 
     it("testa o retorno do FindById", async () => {
-      await baseController.RootFindById(request, response)
-      expect(response.status.calledWith(200)).to.be.equal(true)
-      expect(response.json.calledWith({products: 'sou um teste'}))
-        .to.be.equal(true)
-    })
-  
+      await baseController.RootFindById(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(
+        response.json.calledWith({ products: "sou um teste" })
+      ).to.be.equal(true);
+    });
+
     it("testa o retorno do DeleteById", async () => {
-      await baseController.RootDeleteById(request, response)
-      expect(response.status.calledWith(200)).to.be.equal(true)
-      expect(response.json.calledWith({products: 'sou um teste'}))
-        .to.be.equal(true)
-    })
-  
+      await baseController.RootDeleteById(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(
+        response.json.calledWith({ products: "sou um teste" })
+      ).to.be.equal(true);
+    });
+
     it("testa o retorno do RootInsert", async () => {
-      await baseController.RootInsert(request, response)
-      expect(response.status.calledWith(200)).to.be.equal(true)
-      expect(response.json.calledWith({products: 'sou um teste'}))
-        .to.be.equal(true)
-    })
-
-  })
-
-})
+      await baseController.RootInsert(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(
+        response.json.calledWith({ products: "sou um teste" })
+      ).to.be.equal(true);
+    });
+  });
+});
 
 describe("testa a ProductController", () => {
   let productsController;
@@ -78,62 +77,27 @@ describe("testa a ProductController", () => {
   const request = {};
 
   beforeAll(() => {
+    request.body = { name: "teste", quantity: 400 };
+    request.params = { id: 20 };
 
-    request.body = {name: 'teste', quantity: 400};
-    request.params = {id: 20}
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns();
 
-    response.status = sinon.stub()
-      .returns(response);
-    response.json = sinon.stub()
-      .returns();
-
-    const findAllRes = { status: 200, message: { products: 'sou um teste' } }
-    const product = new ProductsController(new ProductService())
-    sinon.stub(product.service, "Update").callsFake(() => findAllRes)
-    productsController = product
-  })
+    const findAllRes = { status: 200, message: { products: "sou um teste" } };
+    const product = new ProductsController(productService);
+    sinon.stub(product.service, "Update").callsFake(() => findAllRes);
+    productsController = product;
+  });
 
   afterAll(() => {
     product.service.Update.restore;
-  })
+  });
 
   it("testa o retorno do RootUpdateById quando há nome, quantidade e id", async () => {
-    await productsController.RootUpdateById(request, response)
-    expect(response.status.calledWith(200)).to.be.equal(true)
-    expect(response.json.calledWith({products: 'sou um teste'}))
-      .to.be.equal(true)
-  })
-})
-
-describe("testa a SalesController", () => {
-  let salesController;
-  const response = {};
-  const request = {};
-
-  beforeAll(() => {
-
-    request.body = {algo: 400};
-    request.params = {id: 20}
-
-    response.status = sinon.stub()
-      .returns(response);
-    response.json = sinon.stub()
-      .returns();
-
-    const findAllRes = { status: 200, message: { products: 'sou um teste' } }
-    const sales = new SalesController(new ProductService())
-    sinon.stub(sales.service, "Update").callsFake(() => findAllRes)
-    salesController = sales
-  })
-
-  afterAll(() => {
-    sales.service.Update.restore;
-  })
-
-  it("testa o retorno do RootUpdateById quando há nome, quantidade e id", async () => {
-    await salesController.RootUpdateById(request, response)
-    expect(response.status.calledWith(200)).to.be.equal(true)
-    expect(response.json.calledWith({products: 'sou um teste'}))
-      .to.be.equal(true)
-  })
-})
+    await productsController.RootUpdateById(request, response);
+    expect(response.status.calledWith(200)).to.be.equal(true);
+    expect(response.json.calledWith({ products: "sou um teste" })).to.be.equal(
+      true
+    );
+  });
+});
--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -1,138 +1,111 @@
 const sinon = require("sinon");
 const { expect } = require("chai");
 const BaseController = require("../../controllers/Base");
-const ProductService = require('../../services/ProductService')
-const {ProductsController, SalesController} = require("../../controllers");
+const ProductService = require("../../services/ProductService");
+const { ProductsController, SalesController } = require("../../controllers");
 
-describe("testa a ProductController", () => {
-  let productsController;
-  const response = {};
-  const request = {};
-
-  before(() => {
-
-    request.body = {name: 'teste', quantity: 400};
-    request.params = {id: 20}
-
-    response.status = sinon.stub()
-      .returns(response);
-    response.json = sinon.stub()
-      .returns();
-
-    const findAllRes = { status: 200, message: { products: 'sou um teste' } }
-    const product = new ProductsController(new ProductService())
-    sinon.stub(product.service, "Update").callsFake(() => findAllRes)
-    productsController = product
-  })
-
-  after(() => {
-    product.service.Update.restore;
-  })
-
-  it("testa o retorno do RootUpdateById quando há nome, quantidade e id", async () => {
-    await productsController.RootUpdateById(request, response)
-    expect(response.status.calledWith(200)).to.be.equal(true)
-    expect(response.json.calledWith({products: 'sou um teste'}))
-      .to.be.equal(true)
-  })
-})
-
-describe("testa a BaseController", () => {
+const productService = new ProductService();
+describe("testa os controllers", () => {
   let baseController;
-  const response = {};
-  const request = {};
-
-  before(() => {
-
-    request.body = {};
-
-    response.status = sinon.stub()
-      .returns(response);
-    response.json = sinon.stub()
-      .returns();
-
-    const findAllRes = { status: 200, message: { products: 'sou um teste' } }
-    const baseC = new BaseController(new ProductService())
-    sinon.stub(baseC.service, "FindAll").callsFake(() => findAllRes)
-    sinon.stub(baseC.service, "FindById").callsFake(() => findAllRes)
-    sinon.stub(baseC.service, "Delete").callsFake(() => findAllRes)
-    sinon.stub(baseC.service, "InsertOne").callsFake(() => findAllRes)
-    baseController = baseC
-  })
-
-  after(() => {
-    baseC.service.FindAll.restore;
-    baseC.service.FindById.restore;
-    baseC.service.Delete.restore;
-    baseC.service.RootInsert.restore;
-  })
-
-  it("testa o retorno do FindAll", async () => {
-    await baseController.RootFindAll(request, response)
-    expect(response.status.calledWith(200)).to.be.equal(true)
-    expect(response.json.calledWith({products: 'sou um teste'}))
-      .to.be.equal(true)
-  })
-
-  describe("quando passado um id", () => {
-    
-    before(() => {
-      request.params = {id: '300'}
-    })
-
-    it("testa o retorno do FindById", async () => {
-      await baseController.RootFindById(request, response)
-      expect(response.status.calledWith(200)).to.be.equal(true)
-      expect(response.json.calledWith({products: 'sou um teste'}))
-        .to.be.equal(true)
-    })
-  
-    it("testa o retorno do DeleteById", async () => {
-      await baseController.RootDeleteById(request, response)
-      expect(response.status.calledWith(200)).to.be.equal(true)
-      expect(response.json.calledWith({products: 'sou um teste'}))
-        .to.be.equal(true)
-    })
-  
-    it("testa o retorno do RootInsert", async () => {
-      await baseController.RootInsert(request, response)
-      expect(response.status.calledWith(200)).to.be.equal(true)
-      expect(response.json.calledWith({products: 'sou um teste'}))
-        .to.be.equal(true)
-    })
-
-  })
-})
-
-describe("testa a SalesController", () => {
+  let productController;
   let salesController;
   const response = {};
   const request = {};
 
   before(() => {
+    request.body = {};
 
-    request.body = {algo: 400};
-    request.params = {id: 20}
+    response.status = sinon.stub().returns(response);
+    response.json = sinon.stub().returns();
 
-    response.status = sinon.stub()
-      .returns(response);
-    response.json = sinon.stub()
-      .returns();
-
-    const findAllRes = { status: 200, message: { products: 'sou um teste' } }
-    const sales = new SalesController(new ProductService())
-    sinon.stub(sales.service, "Update").callsFake(() => findAllRes)
-    salesController = sales
-  })
+    const findAllRes = { status: 200, message: { products: "sou um teste" } };
+    const baseC = new BaseController(productService);
+    const productC = new ProductsController(productService);
+    const salesC = new SalesController(productService);
+    sinon.stub(baseC.service, "FindAll").callsFake(() => findAllRes);
+    sinon.stub(baseC.service, "FindById").callsFake(() => findAllRes);
+    sinon.stub(baseC.service, "Delete").callsFake(() => findAllRes);
+    sinon.stub(baseC.service, "InsertOne").callsFake(() => findAllRes);
+    sinon.stub(salesC.service, "Update").callsFake(() => findAllRes);
+    baseController = baseC;
+    productController = productC;
+    salesController = salesC;
+  });
 
   after(() => {
-    sales.service.Update.restore;
-  })
+    baseController.service.FindAll.restore;
+    baseController.service.FindById.restore;
+    baseController.service.Delete.restore;
+    baseController.service.InsertOne.restore;
+    productController.service.Update.restore;
+    salesController.service.Update.restore;
+  });
 
-  it("testa o retorno do RootUpdateById quando há nome, quantidade e id", async () => {
-    await salesController.RootUpdateById(request, response)
-    expect(response.status.calledWith(200)).to.be.equal(true)
-    expect(response.json.calledWith({products: 'sou um teste'}))
-      .to.be.equal(true)
-  })
-})
+  it("testa o retorno do FindAll", async () => {
+    await baseController.RootFindAll(request, response);
+    expect(response.status.calledWith(200)).to.be.equal(true);
+    expect(response.json.calledWith({ products: "sou um teste" })).to.be.equal(
+      true
+    );
+  });
+
+  // describe("quando passado um id e body", () => {
+  //   before(() => {
+  //     request.params = { id: "300" };
+  //     request.body = { name: "joao", quantity: 500 };
+  //   });
+
+  //   it("deve retornar uma resposta", async () => {
+  //     await productController.RootUpdateById(request, response);
+  //     expect(response.status.calledWith(200)).to.be.equal(true);
+  //     expect(
+  //       response.json.calledWith({ products: "sou um teste" })
+  //     ).to.be.equal(true);
+  //   });
+  // });
+
+  describe("quando passado um id", () => {
+    before(() => {
+      request.params = { id: "300" };
+    });
+
+    it("testa o retorno do FindById", async () => {
+      await baseController.RootFindById(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(
+        response.json.calledWith({ products: "sou um teste" })
+      ).to.be.equal(true);
+    });
+
+    it("testa o retorno do DeleteById", async () => {
+      await baseController.RootDeleteById(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(
+        response.json.calledWith({ products: "sou um teste" })
+      ).to.be.equal(true);
+    });
+
+    it("testa o retorno do RootInsert", async () => {
+      await baseController.RootInsert(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(
+        response.json.calledWith({ products: "sou um teste" })
+      ).to.be.equal(true);
+    });
+  });
+
+  describe("quando passado um id e body", () => {
+    before(() => {
+      request.params = { id: "300" };
+      request.body = { name: "joao", quantity: 500 };
+    });
+
+    it("deve retornar uma resposta", async () => {
+      await salesController.RootUpdateById(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(
+        response.json.calledWith({ products: "sou um teste" })
+      ).to.be.equal(true);
+    });
+  });
+});
