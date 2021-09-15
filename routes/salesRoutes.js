198ff122b5419a75855f7121a57505f1bf2ac390
Lara Capila <laracapilak@gmail.com>
2021-09-15 10:07:43 -0300

    Adiciona rotas. Rota para pegar todas as vendas já funcionando

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -4,6 +4,9 @@ const salesController = require('../controllers/salesController');
 
 const router = Router();
 
+router.get('/sales/:id', () => console.log('get by id'));
+router.get('/sales', salesController.AllSales);
+
 router.post('/sales',
 salesController.validQuantity,
 salesController.creteSales);
