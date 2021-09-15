6b8183fd3fe28408982908275b6ef9ac86700b1a
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-15 01:21:36 -0300

    feat: :sparkles: implements validation that does not allow a product with the same name as an existing one

--- services/productService.js
+++ services/productService.js
@@ -1,33 +1,33 @@
 const productModel = require('../models/productModel');
 
-const validateName = (name) => {
-  const result = {
+const validateName = async (name) => {
+  let message = null;
+
+  if (name.length < 5) {
+    message = '"name" length must be at least 5 characters long';
+    return message;
+  }
+ 
+  if (await productModel.productExists(name)) {
+    message = 'Product already exists';
+    return message;
+  }
+  
+  return message;
+};
+
+const createProduct = async ({ name, quantity }) => {
+  const message = await validateName(name);
+  if (message) {
+   return {
     status: 422,
     messageResult: {
       err: {
         code: 'invalid_data',
-        message: '', 
+        message, 
       },
     },
-  };
-
-  if (name.length < 5) {
-    result.messageResult.err.message = '"name" length must be at least 5 characters long';
-    return result;
-  }
-
-  if (productModel.productExists) {
-    result.messageResult.err.message = 'Product already exists';
-    return result;
-  }
-  
-  return null;
-};
-
-const createProduct = async ({ name, quantity }) => {
-  const result = validateName(name);
-  if (result) {
-    return result;
+   };
   }
 
   const { ops } = await productModel.createProduct({ name, quantity });
