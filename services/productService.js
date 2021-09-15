374416f6ea1e27348a4da0fb25c7e897a61c448b
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-14 21:10:42 -0300

    feat: :sparkles: Implements the validation that prevents creating a product with a name less than 5 characters

--- services/productService.js
+++ services/productService.js
@@ -1,6 +1,18 @@
 const productModel = require('../models/productModel');
 
 const createProduct = async ({ name, quantity }) => {
+  if (name.length < 5) {
+    return {
+      status: 422,
+      messageResult: {
+        err: {
+          code: 'invalid_data',
+          message: '"name" length must be at least 5 characters long', 
+        },
+      },
+    };
+  }
+
   const { ops } = await productModel.createProduct({ name, quantity });
   return {
     status: 201,
