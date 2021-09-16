5bd2a19869c2b791c8c04e13f3bbeb7e94b87603
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-16 07:27:49 -0300

    feat: :sparkles: implements productRouter.get with address '/' in productController

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,12 @@ const productService = require('../services/productService');
 
 const productRouter = express.Router();
 
+productRouter.get('/', async (_req, res) => {
+  const { status, messageResult } = await productService.getAllProducts();
+
+  return res.status(status).json(messageResult);
+});
+
 productRouter.post('/', async (req, res) => {
   const { name, quantity } = req.body;
   const { status, messageResult } = await productService.createProduct({ name, quantity });
