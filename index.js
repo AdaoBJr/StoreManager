fa5883f4759b75928ed24ad208cf4642a02a6c89
isTorres <ismaeltorres612@gmail.com>
2021-09-16 00:59:19 -0300

    edit routs and add middlers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const { quantityArray } = require('./schemas/schemasValidate');
 const Product = require('./controller/controllerProducts');
 const Sales = require('./controller/controllerSales');
 
@@ -18,23 +20,19 @@ app.get('/products', Product.getAll);
 app.post('/products', Product.create);
 
 // Sales
-// app.delete('/sales/:id', Sales.deleteSale);
+app.delete('/sales/:id', Sales.deleteSale);
 
-app.put('/sales/:id', Sales.update);
+app.put('/sales/:id', quantityArray, Sales.update);
 
 app.get('/sales/:id', Sales.getById);
 
 app.get('/sales', Sales.getAll);
 
-app.post('/sales', Sales.create);
+app.post('/sales', quantityArray, Sales.create);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
-
-// app.use((err, req, res, _next) => {
-//   res.status(500).json({ error: `Erro: ${err.message}` });
-// });
 
 app.listen(3000, () => console.log('Aplicação ouvindo na porta 3000'));
