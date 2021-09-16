7230dd2fa92fb0db8211e94a1a4d501256955f09
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-16 15:23:21 -0300

    Refactoring and adding a new route to router

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const productsRouter = require('./src/routers/productsRouter');
+const salesRouter = require('./src/routers/salesRouter');
 
 const { errorMiddleware } = require('./errorMiddleware');
 
@@ -17,20 +18,8 @@ app.get('/', (_request, response) => {
 
 app.use('/products', productsRouter);
 
+app.use('/sales', salesRouter);
+
 app.use(errorMiddleware);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
-
-// Caso o recurso não seja encontrado, sua API retorne o status HTTP adequado com o body { message: '<recurso> não encontrado' }.
-
-// Em caso de erro, sua API retorne o status HTTP adequado com o body { err: { message: <mensagem de erro>, code: <código do erro> } }.
-
-// O código do erro deve ser determinado por você e deve seguir o mesmo padrão para toda a aplicação. Por exemplo: 'not_found', 'invalid_data' e afins.
-
-// Em caso de dados inválidos, sua API retorne o status HTTP adequado, com o body { err: { message: 'Dados inválidos', code: <código do erro> } }.
-
-// Todos os retornos de erro devem seguir o mesmo formato. Para erros que requerem dados adicionais (por exemplo, para informar quais campos estão incorretos) utilize a propriedade data dentro do objeto err.
-
-// gerar os objetos de erro personalizados, biblioteca de erros, boom.
-
-// Joi
