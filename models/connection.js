9cd43b354c9e87f8fe4a06ff243d3ee003824c55
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-16 04:48:10 -0300

    feat: :goal_net: handles error when connecting to database

--- models/connection.js
+++ models/connection.js
@@ -14,7 +14,12 @@ const DB_NAME = 'StoreManager';
 
 const getConnection = async () => {
   connection = connection || await MongoClient.connect(MONGO_DB_URL, OPTIONS)
-  .then((conn) => conn.db(DB_NAME));
+  .then((conn) => conn.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+  
   return connection;
 };
 
