16b1986c0699a10fb51f9f49aed88891eb59df09
Victor Lopes <victor.cabrera0209@gmail.com>
2021-09-16 15:34:24 -0300

    Adding a route to get all sales

--- src/routers/salesRouter.js
+++ src/routers/salesRouter.js
@@ -4,6 +4,10 @@ const salesController = require('../controllers/salesController');
 
 const router = Router();
 
+router.get('/', salesController.getAllSales);
+
+router.get('/:id', salesController.getSaleById);
+
 router.post('/', salesController.addSale);
 
 module.exports = router;
