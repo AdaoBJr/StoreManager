bb4f7c35adcc05f1c905bac6dbd9fd40f0b316e2
Thiago Felipe <tfeliperibeiro@pop-os.localdomain>
2021-09-16 17:45:22 -0300

    Erro 1

--- models/connection.js
+++ models/connection.js
@@ -9,7 +9,7 @@ useNewUrlParser: true,
 useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 let db = null;
 
