9972682a0af734c7b0c520abf09e5cdc6796f506
Lara Capila <laracapilak@gmail.com>
2021-09-16 14:37:56 -0300

    Cria rota para delete

--- routes/salesRoutes.js
+++ routes/salesRoutes.js
@@ -8,6 +8,8 @@ router.put('/sales/:id',
 salesController.validQuantity,
 salesController.editSale);
 
+router.delete('/sales/:id', salesController.deleteSale);
+
 router.get('/sales/:id', salesController.validId);
 router.get('/sales', salesController.AllSales);
 
