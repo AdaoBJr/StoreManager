cdd1249153bfd8df1a36ccd6baf6dc34b4d5b904
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-17 19:34:10 -0300

    feat: :sparkles: implements getSaleById in saleModel

--- models/saleModel.js
+++ models/saleModel.js
@@ -1,4 +1,4 @@
-// const { ObjectId } = require('mongodb');
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const createSale = async ({ itensSold }) => connection.getConnection()
@@ -8,7 +8,17 @@ const createSale = async ({ itensSold }) => connection.getConnection()
 const getAllSales = async () => connection.getConnection()
   .then((db) => db.collection('sales').find().toArray());
 
+const getSaleById = async (id) => {
+  if (!ObjectId.isValid(id)) {
+    return null;
+  }
+
+  return connection.getConnection()
+    .then((db) => db.collection('sales').findOne({ _id: ObjectId(id) }));
+};
+
 module.exports = {
   createSale,
   getAllSales,
+  getSaleById,
 };
