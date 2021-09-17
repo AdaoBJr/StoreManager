3dda941d19871c9f179e9d380aa6b8e91d57547a
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-17 18:58:49 -0300

    feat: :sparkles: implements createSale in saleService

---
+++ services/saleService.js
@@ -0,0 +1,29 @@
+const saleModel = require('../models/saleModel');
+
+const isQuantityValid = (quantity) => (typeof quantity === 'number') && (quantity > 0);
+
+const createSale = async ({ itensSold }) => {
+  const quantityValid = itensSold.every(({ quantity }) => isQuantityValid(quantity));
+  
+  if (!quantityValid) {
+   return {
+    status: 422,
+    messageResult: {
+      err: {
+        code: 'invalid_data',
+        message: 'Wrong product ID or invalid quantity', 
+      },
+    },
+   };
+  }
+
+  const messageResult = await saleModel.createSale({ itensSold });
+  return {
+    status: 200,
+    messageResult,
+  };
+};
+
+module.exports = {
+  createSale,
+};
