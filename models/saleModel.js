a61d37f21e4367a6c5e219e819c9b68d82f8683d
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-09-17 19:09:21 -0300

    feat: :sparkles: implements getAllSales in saleModel

--- models/saleModel.js
+++ models/saleModel.js
@@ -5,6 +5,13 @@ const createSale = async ({ itensSold }) => connection.getConnection()
   .then((db) => db.collection('sales').insertOne({ itensSold }))
   .then((result) => result.ops[0]);
 
+const getAllSales = async () => {
+  const result = await connection.getConnection()
+  .then((db) => db.collection('sales').find().toArray());
+  return { sales: result };
+};
+
 module.exports = {
   createSale,
+  getAllSales,
 };
