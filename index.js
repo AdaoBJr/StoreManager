e35424b3e90d0d11e989d4b72e0320998aadbe77
Bruno Wedley <brunowedley27@gmail.com>
2021-09-18 17:18:51 -0300

    correçoes no lint

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const express = require('express');
+
+const app = express();
+const route = require('./src/routes/routes');
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(route);
+
+app.listen(PORT, () => {
+  console.log('Fala que eu te escuto');
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
---
+++ src/controllers/ProductsController.js
@@ -0,0 +1,88 @@
+const rescue = require('express-rescue');
+const service = require('../services/ProductsService');
+
+const CODE_200 = 200;
+const CODE_201 = 201;
+const ERROR_CODE_422 = 422;
+// const ERROR_CODE_404 = 404;
+
+const createProduct = async (req, res) => {
+  const { name, quantity } = req.body;
+  try {
+    const newProduct = await service.create(name, quantity);
+
+    res.status(CODE_201).json(newProduct);
+  } catch (e) {
+    res.status(ERROR_CODE_422).json({ err: 
+      { 
+        code: 'invalid_data',
+        message: e.message,
+      },
+    });
+  }
+};
+
+const getAllProducts = rescue(async (_req, res) => {
+  const products = await service.getAll();
+
+  res.status(CODE_200).json({ products });
+});
+
+const getProductById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await service.getById(id);  
+
+    res.status(CODE_200).json(result);
+  } catch (e) {
+    res.status(ERROR_CODE_422).json({ err: 
+      { 
+        code: 'invalid_data',
+        message: e.message,
+      },
+    });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const { name, quantity } = req.body;
+    const { id } = req.params;
+
+    const result = await service.updateOne(id, name, quantity);
+
+    res.status(CODE_200).json(result);
+  } catch (e) {
+    res.status(ERROR_CODE_422).json({ err: 
+      { 
+        code: 'invalid_data',
+        message: e.message,
+      },
+    });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const notProduct = await service.deleteOne(id);
+
+    res.status(CODE_200).json(notProduct);
+  } catch (e) {
+    res.status(ERROR_CODE_422).send({ err: 
+      { 
+        code: 'invalid_data',
+        message: e.message,
+      },
+    });
+  }
+};
+
+module.exports = {
+  deleteProduct,
+  createProduct, 
+  getProductById, 
+  getAllProducts,
+  updateProduct,
+};
