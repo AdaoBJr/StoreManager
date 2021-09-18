15642939a68bd371f2e8b9f2d502f6077b4e0cbb
Víctor Cançado <victor.cancado@gmail.com>
2021-09-18 01:16:11 -0300

    corrige banco de dados

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,10 @@
 const mongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+
 const connection = () => mongoClient
-  .connect(process.env.MONGO_DB_URL, {
+  .connect(MONGO_DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
