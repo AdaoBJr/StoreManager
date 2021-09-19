73e7521a600d42cbbd27c0779860d4cf2d741ac7
CIGR2021 <appencigr@gmail.com>
2021-09-19 13:28:29 -0300

    Criei a rota put update

--- routes/sales.js
+++ routes/sales.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const Sale = require('../controllers/sales');
+const controllerSale = require('../controllers/sales');
 
 const sales = express.Router();
 
-sales.post('/sales', Sale.create);
-sales.get('/sales', Sale.getAll);
-sales.get('/sales/:id', Sale.getById);
+sales.post('/sales', controllerSale.create);
+sales.get('/sales', controllerSale.getAll);
+sales.get('/sales/:id', controllerSale.getById);
+sales.put('/sales/:id', controllerSale.update);
 
 module.exports = sales;
