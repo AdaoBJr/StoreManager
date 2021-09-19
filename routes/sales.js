4dc1a2a14958849ba44c1a68de7daa4a8fe63372
CIGR2021 <appencigr@gmail.com>
2021-09-19 13:11:14 -0300

    Criei as rotas get do sales no models

--- routes/sales.js
+++ routes/sales.js
@@ -4,5 +4,7 @@ const Sale = require('../controllers/sales');
 const sales = express.Router();
 
 sales.post('/sales', Sale.create);
+sales.get('/sales', Sale.getAll);
+sales.get('/sales/:id', Sale.getById);
 
 module.exports = sales;
