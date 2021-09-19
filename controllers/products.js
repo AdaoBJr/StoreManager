0da7052c000f025d36cf917262246abd2b095ff8
CIGR2021 <appencigr@gmail.com>
2021-09-19 11:57:24 -0300

    Requisito 2 - Fix

--- controllers/products.js
+++ controllers/products.js
@@ -14,7 +14,7 @@ const getAll = async (_req, res) => {
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const { code, err, product } = await serviceProduct.getProductById(id);
+  const { code, err, product } = await serviceProduct.getById(id);
   if (err) return res.status(code).json({ err });
   return res.status(code).json(product);
 };
