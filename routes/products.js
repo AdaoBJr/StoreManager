0c23926387f09b0f6331103700746a643e7c0a9e
CIGR2021 <appencigr@gmail.com>
2021-09-19 11:50:35 -0300

    Adicionei a rota put update

--- routes/products.js
+++ routes/products.js
@@ -6,5 +6,6 @@ const products = express.Router();
 products.post('/products', controllerProduct.create);
 products.get('/products', controllerProduct.getAll);
 products.get('/products/:id', controllerProduct.getById);
+products.put('/products/:id', controllerProduct.update);
 
 module.exports = products;
