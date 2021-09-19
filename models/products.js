403fb28a11e1f855a3eea30512b8feb96ab4f189
CIGR2021 <appencigr@gmail.com>
2021-09-19 11:41:33 -0300

    Requisito 2 - Fix Eslint

--- models/products.js
+++ models/products.js
@@ -1,5 +1,5 @@
-const connection = require('./connection');
 const { ObjectId } = require('mongodb');
+const connection = require('./connection');
 
 const create = async (product) => {
   const db = await connection();
