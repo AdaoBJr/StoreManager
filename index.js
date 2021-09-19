de820ae58002611c88dbb4e62794ea5665b52a3a
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-09-19 19:50:36 -0300

    Add wait-on hook

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
+app.get('/', (_request, response) => response.send());
 app.get('/products', getAllProducts);
 app.get('/products/:id', getProductById);
 app.post('/products', addNewProduct);
