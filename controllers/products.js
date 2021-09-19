9cc603edf07d0c8d380f63d751018a28793c2a36
CIGR2021 <appencigr@gmail.com>
2021-09-19 10:35:14 -0300

    Criei a funçao create no controller

---
+++ controllers/products.js
@@ -0,0 +1,10 @@
+const serviceProduct = require('../services/products');
+
+const create = async (req, res) => {
+  const { name, quantity } = req.body;
+  const { code, err, newProduct } = await serviceProduct.create({ name, quantity });
+  if (err) return res.status(code).json({ err });
+  return res.status(code).json(newProduct);
+};
+
+module.exports = create;
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-require('module-alias/register');
+const express = require('express');
 
-const config = require('@config');
-const app = require('@app');
+const app = express();
+app.use(express.json());
+const PORT = 3000,
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.listen(config.app.port, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     return console.log('erro');
   }
-  console.log('iniciou em http://localhost:3000');
+  console.log(`iniciou em http://localhost:${PORT}`);
 });
