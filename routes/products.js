b7ec7ef615b9e54590e8eb8fae82ab88c096b83b
CIGR2021 <appencigr@gmail.com>
2021-09-19 12:13:46 -0300

    Adicionei a rota delete no exclude

--- routes/products.js
+++ routes/products.js
@@ -7,5 +7,6 @@ products.post('/products', controllerProduct.create);
 products.get('/products', controllerProduct.getAll);
 products.get('/products/:id', controllerProduct.getById);
 products.put('/products/:id', controllerProduct.update);
+products.delete('/products/:id', controllerProduct.exclude);
 
 module.exports = products;
