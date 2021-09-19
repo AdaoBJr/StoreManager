303dea9d3b19cad6efbfb8e4bab2b3acc58dab56
CIGR2021 <appencigr@gmail.com>
2021-09-19 11:25:37 -0300

    Criei as funçoes: getAll e getById no models

--- models/products.js
+++ models/products.js
@@ -1,5 +1,17 @@
 const connection = require('./connection');
 
+const create = async (product) => {
+  const db = await connection();
+  const newProduct = db.collection('products').insertOne(product);
+  return { _id: newProduct.insertedId, ...product };
+};
+
+const getAll = async () => {
+  const db = await connection();
+  const allProducts = await db.collection('products').find().toArray();
+  return allProducts;
+};
+
 const getByName = async (name) => {
   const db = await connection();
   const foundProduct = await db.collection('products').findOne({ name });
@@ -7,13 +19,14 @@ const getByName = async (name) => {
   return true;
 };
 
-const create = async (product) => {
+const getById = async (id) => {
   const db = await connection();
-  const newProduct = db.collection('products').insertOne(product);
-  return { _id: newProduct.insertedId, ...product };
+  return db.collection('products').findOne(ObjectId(id));
 };
 
 module.exports = {
   create,
   getByName,
+  getAll,
+  getById,
 };
