f8199fb994a7db15ec65a321a97db204104ac6db
Aderson Santos <assaderson@gmail.com>
2021-09-19 19:44:41 -0300

    Incluindo o index.js

--- index.js
+++ index.js
@@ -3,3 +3,14 @@
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const productController = require('./controllers/productsController');
+const salesController = require('./controllers/salesController');
+const app = express();
+const PORT = 3000;
+app.use(bodyParser.json());
+app.listen(PORT, () => {
+  console.log(`App está funcionando na porta ${PORT}`);
+});
