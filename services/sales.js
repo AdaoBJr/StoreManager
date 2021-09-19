a9495c048579e76d8864d215c032a5418ecf089d
CIGR2021 <appencigr@gmail.com>
2021-09-19 13:19:14 -0300

    Criei as funções: getAll e getById no sales services

--- services/sales.js
+++ services/sales.js
@@ -1,11 +1,40 @@
 const modelSales = require('../models/sales');
-const validate = require('../schemas/sales');
+const {
+  isQuantityValid,
+  isIdValid,
+} = require('../schemas/sales');
 
 const create = async (sales) => {
-  const isValid = validate.isQuantityValid(sales);
+  const isValid = isQuantityValid(sales);
   if (isValid.err) return isValid;
   const newSale = await modelSales.create(sales);
   return { code: 200, newSale };
 };
 
-module.exports = { create };
+const getAll = async () => {
+  const sales = await modelSales.getAll();
+  return { code: 200, sales };
+};
+
+const getById = async (id) => {
+  const idValid = isIdValid(id);
+  if (idValid.err) return idValid;
+  console.log('aaaa');
+  const sale = await modelSales.getById(id);
+
+  if (!sale) {
+    return {
+      code: 404,
+      err: {
+        code: 'not_found', message: 'Sale not found',
+      },
+    };
+  }
+  return { code: 200, sale };
+};
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+};
