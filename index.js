93653d65bf29acc655d9065f0bb6af73b13fcbdd
CIGR2021 <appencigr@gmail.com>
2021-09-19 12:51:44 -0300

    Requisito 5 - Feito

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const app = express();
 const products = require('./routes/products');
+const sales = require('./routes/sales');
 
 app.use(express.json());
 const PORT = 3000;
 
 app.use(products);
+app.use(sales);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
