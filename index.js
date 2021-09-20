9dc644def4009bb89b8ddeca593bd36d4cd3b3d9
Pedro Toselli <pedrotrybe@gmail.com>
2021-09-20 19:22:41 -0300

    Iniciando Projeto novamenste segundo as regras do MSC

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const productRouter = require('./products/productRoutes');
-const salesRouter = require('./sales/salesRoutes');
-
 const app = express();
 const PORT = '3000';
 
 app.use(bodyParser.json());
-
-app.use('/products', productRouter);
-app.use('/sales', salesRouter);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
