0945ad3e99e0d0ee7af62ad60080e680e4ab5ad4
Víctor Cançado <victor.cancado@gmail.com>
2021-09-20 14:12:21 -0300

    req2 corrigido

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const mongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
-const { MONGO_DB_URL } = process.env;
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 // process.env.MONGO_DB_URL;
 // || 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
