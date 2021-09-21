c83c54b01dbd02d6c2d0a0bdf437621a844c1b4f
walterlbf <walter.lbf.f@gmail.com>
2021-09-21 12:01:19 -0400

    referencias de estudo

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ app.use(salesRoutes);
 app.listen(3000, () => {
   console.log('Rodando na porta 3000');
 });
+
+// utilizei o repositorio do Emerson Saturnino para consulta: https://github.com/tryber/sd-010-b-store-manager/pull/32
