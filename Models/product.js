ec173cb565b3b77631c24955630d323e0cc37112
Paulo Freire <paulofreire.si@outlook.com>
2021-09-27 20:21:49 -0300

    minor space errors

--- Models/product.js
+++ Models/product.js
@@ -1,15 +1,15 @@
 const connection = require('./connections');
 
-const create = async (name, quantity) => 
-    connection()
-        .then((db) => db.collection('products').insertOne({ name, quantity }))
-        .then((result) => ({ _id: result.insertedId, name, quantity }));
+const create = async (name, quantity) =>
+  connection()
+    .then((db) => db.collection('products').insertOne({ name, quantity }))
+    .then((result) => ({ _id: result.insertedId, name, quantity }));
 
 const find = async (name) => {
-    const product = await connection()
-        .then((db) => db.collection('products').findOne({ name }));
-    if (!product) return null;
-    return product;
+  const product = await connection()
+    .then((db) => db.collection('products').findOne({ name }));
+  if (!product) return null;
+  return product;
 };
 
 module.exports = {
