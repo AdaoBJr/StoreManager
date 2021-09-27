0882fd6d987d84d0dcfebe7437fbccca75a6bd46
Paulo Freire <paulofreire.si@outlook.com>
2021-09-27 11:35:00 -0300

    creating my controllers

--- Controllers/products.js
+++ Controllers/products.js
@@ -1,1 +1,10 @@
-//testing
+const service = '';
+// require('../services/products');
+
+const create = async (req, res) => {
+    const { name, quantity } = req.body;
+    const result = await service.create(name, quantity);
+    return res.status(201).json(result);
+};
+
+module.exports = create;
