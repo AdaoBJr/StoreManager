4b54a88156b6ee8a141d5d35f81cf46fb3122669
Paulo Freire <paulofreire.si@outlook.com>
2021-09-27 18:39:10 -0300

    errors in space

--- Middleware/error.js
+++ Middleware/error.js
@@ -1,30 +1,30 @@
 const errors = {
     name: {
-        code: 'invalid_data',
-        message: '"name" length must be at least 5 characters long',
-        status: 422,
+      code: 'invalid_data',
+      message: '"name" length must be at least 5 characters long',
+      status: 422,
     },
-
+  
     exist: {
-        code: 'invalid_data',
-        message: 'Product already exists',
-        status: 422,
+      code: 'invalid_data',
+      message: 'Product already exists',
+      status: 422,
     },
-
+  
     quantity: {
-        code: 'invalid_data',
-        message: '"quantity" must be larger than or equal to 1',
-        status: 422,
+      code: 'invalid_data',
+      message: '"quantity" must be larger than or equal to 1',
+      status: 422,
     },
-
+  
     quantityType: {
-        code: 'invalid_data',
-        message: '"quantity" must be a number',
-        status: 422,
+      code: 'invalid_data',
+      message: '"quantity" must be a number',
+      status: 422,
     },
-};
-
-module.exports = (err, _req, res, _next) => {
+  };
+  
+  module.exports = (err, _req, res, _next) => {
     console.log(err);
     console.log(errors[err.statusCode]);
     if (err.statusCode) {
@@ -32,4 +32,4 @@ module.exports = (err, _req, res, _next) => {
       return res.status(status).json({ err: { code, message } });
     }
     return res.status(500).json({ message: err.message });
-};
+  };
