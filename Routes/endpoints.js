1c8897d77d598849c1caa66b6f4d2327047f6716
Paulo Freire <jeveaux995@gmail.com>
2021-09-27 11:21:56 -0300

    creating my routes

---
+++ Routes/endpoints.js
@@ -0,0 +1,9 @@
+const rescue = require('express-rescue');
+const controller = require('../Controllers/products');
+
+const post2Create = (app) => {
+    app.route('/products')
+    .post(rescue(controller.create));
+};
+
+module.exports = post2Create;
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require('express');
+
+const app = express();
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
