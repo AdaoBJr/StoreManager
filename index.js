d561381489a4897b6e0d3feff84da03806a213c8
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-09-28 16:14:45 -0300

    Adiciona connection ao banco

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Conexão local
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+const PORT = '3000';
+
+// Conexão avaliador
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.post('/products', (_request, response) => {
+  response.status(201).send();
+});
+
+app.listen(PORT, () => {
+  console.log('Online');
 });
