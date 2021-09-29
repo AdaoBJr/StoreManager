2ddc399e2eb7ca7f1ed351c460206d411006baaf
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-09-28 21:49:56 -0300

    Exporta a conexão como objeto

--- model/connection.js
+++ model/connection.js
@@ -1,16 +1,21 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = process.env.DB_URL || 'mongodb://mongodb:27017/StoreManager';
-const DB_NAME = process.env.DB_NAME || 'StoreManager';
+// Uso local
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// Enviar para avaliador
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
-const connection = async () => MongoClient
-        .connect(MONGO_DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then((conn) => conn.db(DB_NAME))
-        .catch((err) => {
-            console.error(err);
-        });
+const DB_NAME = 'StoreManager';
 
-module.exports = connection;
+const connection = () => MongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,        
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+
+module.exports = { connection };
