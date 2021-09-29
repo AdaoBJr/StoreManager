b23786cd66d65959013e4d5eeb718b5e345c90ee
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-09-28 23:36:54 -0300

    Tira commit do test models e adiciona sinon restore

--- test/unit/models.js
+++ test/unit/models.js
@@ -1,48 +1,47 @@
-// const { MongoMemoryServer } = require
-// ('mongodb-memory-server-core');
-// const { MongoClient, ObjectId } = require('mongodb');
-// const sinon = require('sinon');
-// const { expect } = require('chai');
+const { MongoMemoryServer } = require
+('mongodb-memory-server-core');
+const { MongoClient, ObjectId } = require('mongodb');
+const sinon = require('sinon');
+const { expect } = require('chai');
 
-// const mongoConnection = require('../../model/connection')
-// const productsModel = require('../../model/productsModel')
+const mongoConnection = require('../../model/connection')
+const productsModel = require('../../model/productsModel')
 
-// describe('Insere um novo produto no BD', () => {
-// 	const newProduct = {
-//     name: "UmProduto",
-//     Quantity: "100"
-//   }
+describe('Insere um novo produto no BD', () => {
+	const newProduct = {
+    name: "UmProduto",
+    Quantity: "100"
+  }
 
-//   let DBServer = new MongoMemoryServer();
-//   let connectionMock;
+  let DBServer = new MongoMemoryServer();
+  let connectionMock;
   
-//   before(async () => {
-//     const URLMock = await DBServer.getUri();
-//     connectionMock = await MongoClient.
-//     connect(URLMock, {
-//       useNewUrlParser: true,
-// 			useUnifiedTopology: true,
-//     })
-//     .then(conn => conn.db('StoreManager'));
+  before(async () => {
+    const URLMock = await DBServer.getUri();
+    connectionMock = await MongoClient.
+    connect(URLMock, {
+      useNewUrlParser: true,
+			useUnifiedTopology: true,
+    })
+    .then(conn => conn.db('StoreManager'));
 
-//     sinon.stub(mongoConnection, 'connection').resolves(connectionMock);
-//   })
+    sinon.stub(mongoConnection, 'connection').resolves(connectionMock);
+  })
 
-//   after(async () => {
-// 		await mongoConnection.connection.restore();
-//     await DBServer.stop();
-// 	});
+  after(async () => {
+    sinon.restore()
+	});
 
-//   describe('Quando é inserido com sucesso', () => {
-//     it('Retorna um objeto', async () => {
-//       const response = await productsModel.create(newProduct)
-//       expect(response).to.be.a('object');
-//     })
+  describe('Quando é inserido com sucesso', () => {
+    it('Retorna um objeto', async () => {
+      const response = await productsModel.create(newProduct)
+      expect(response).to.be.a('object');
+    })
 
-//     it('O objeto possui o "id" do novo produto', async () => {
-//       const response = await productsModel.create(newProduct);
+    it('O objeto possui o "id" do novo produto', async () => {
+      const response = await productsModel.create(newProduct);
 
-//       expect(response).to.have.a.property('id');
-//     });
-//   })
-// });
+      expect(response).to.have.a.property('id');
+    });
+  })
+});
