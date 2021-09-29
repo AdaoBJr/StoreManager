832080c4942d4772910bdaad81c75fa71706d3be
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-09-29 11:42:38 -0300

    Remove console log

--- test/unit/controllers.js
+++ test/unit/controllers.js
@@ -4,8 +4,6 @@ const { expect } = require('chai');
 const productController = require('../../controllers/productController')
 const productService = require('../../services/productsService')
 
-console.log("carreguei o script");
-
 describe('Verifica se retorna as informações corretas ao criar produto', () => {
   const res = {};
   const req = {};
@@ -16,8 +14,6 @@ describe('Verifica se retorna as informações corretas ao criar produto', () =>
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns();
   })
-
-  console.log("Entrei no primeiro describe");
 
   it('Quando o payload é inválido', async () => {
     await productController.createNewProduct(req, res)
@@ -52,8 +48,6 @@ describe('Quando o payload é válido', async () => {
   after(() => {
     productService.createNewProduct.restore();
   });
-
-  console.log("Entrei no segundo describe");
 
   it('Quando o payload é válido', async () => {
     await productController.createNewProduct(req, res)
