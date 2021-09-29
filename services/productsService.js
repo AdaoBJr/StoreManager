4b5e866a1968a7ece8442d9add40ddb9dc582859
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-09-28 23:00:53 -0300

    Separa validateProduct da createNewProduct

--- services/productsService.js
+++ services/productsService.js
@@ -1,20 +1,26 @@
 const Joi = require('joi');
 const productsModel = require('../model/productsModel');
 
-const createNewProduct = async (name, quantity) => {
+const validateProduct = async (name, quantity) => {
   const { error } = Joi.object({
     name: Joi.string().min(5),
     quantity: Joi.number().integer().min(1),
   })
   .validate({ name, quantity });
+  
+  if (error) return error.details[0].message;
 
-  if (error) console.log('Error no createNewProduct do productService');
+  const alreadyExists = await productsModel.findByName(name);
+  if (alreadyExists) return 'Product already exists';
+};
 
+const createNewProduct = async (name, quantity) => {
   const newProduct = await productsModel.create(name, quantity);
 
-  return newProduct;
+  return { newProduct };
 };
 
 module.exports = {
   createNewProduct,
+  validateProduct,
 };
