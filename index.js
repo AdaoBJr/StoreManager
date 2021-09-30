37efa1237708747a161ca954eaaf443f01d46b56
Uibirá <usbarreto@gmail.com>
2021-09-30 16:46:55 -0300

    Novamente

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
-const PORT = '3000';
+const PORT = process.env.PORT || 3000;
+// const PORT = '3000';
 
 const StoreController = require('./controllers/storeController');
-// const SalesController = require('./controllers/salesController');
-// const ErrorMiddleware = require('./middlewares/error.js');
+const SalesController = require('./controllers/salesController');
+const ErrorMiddleware = require('./middlewares/error.js');
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -17,16 +18,16 @@ app.get('/', (_request, response) => {
 });
 
 app.post('/products', StoreController.create);
-// app.get('/products/:id', StoreController.getByIdOrName);
-// app.get('/products', StoreController.getAll);
-// app.put('/products/:id', StoreController.updateById);
-// app.delete('/products/:id', StoreController.deleteById);
-// app.post('/sales', SalesController.create);
-// app.get('/sales/:id', SalesController.getById);
-// app.get('/sales', SalesController.getAll);
-// app.put('/sales/:id', SalesController.updateById);
-// app.delete('/sales/:id', SalesController.deleteById);
+app.get('/products/:id', StoreController.getByIdOrName);
+app.get('/products', StoreController.getAll);
+app.put('/products/:id', StoreController.updateById);
+app.delete('/products/:id', StoreController.deleteById);
+app.post('/sales', SalesController.create);
+app.get('/sales/:id', SalesController.getById);
+app.get('/sales', SalesController.getAll);
+app.put('/sales/:id', SalesController.updateById);
+app.delete('/sales/:id', SalesController.deleteById);
 
-// app.use(ErrorMiddleware);
+app.use(ErrorMiddleware);
 
 app.listen(PORT, () => { console.log(`Ouvindo a porta ${PORT}`); });
