2df3320827a18700faa28de9e2305a3cfce96fd4
Eduardo Bonizio <eduardo.bonizio@gmail.com>
2021-09-30 18:08:52 -0300

    Corrige funcao validateProductsArray

--- services/salesService.js
+++ services/salesService.js
@@ -5,8 +5,8 @@ const salesModel = require('../models/salesModel');
 
 const validateId = async (id) => {
   const isValidId = ObjectID.isValid(id);
-
   if (!isValidId) return true;
+
   return false;
 };
 
@@ -22,20 +22,19 @@ const validateQuantity = (quantity) => {
 
 const isProductOnDb = async (productId) => {
   const found = await productsModel.findById(productId);
-
   if (found) return true;
   return false;
 };
 
 const validateProductsArray = async (salesArray) => {
   let error;
-  await salesArray.forEach(async (element) => {
+  salesArray.forEach((element) => {
     const { productId, quantity } = element;
-    const invalidId = await validateId(productId);
+    const invalidId = validateId(productId);
     const invalidQuantity = validateQuantity(quantity);
-    // Inverte o resultado para que error seja true caso o produto não exista
-    const inexistentProduct = !isProductOnDb(productId);
-    if (invalidId || invalidQuantity || inexistentProduct) {
+    const productExists = isProductOnDb(productId);
+    // Inverte o productExists para que error seja true caso o produto não exista
+    if (invalidId || invalidQuantity || !productExists) {
       error = { errorMessage: { err: { 
         code: 'invalid_data', message: 'Wrong product ID or invalid quantity' }, 
       } };
