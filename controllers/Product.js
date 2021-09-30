c0e8d5de93c70783fc643d6212a7ace031bf7538
Uibirá <usbarreto@gmail.com>
2021-09-30 18:18:28 -0300

    Teste de correçao no controller de produtos

--- controllers/Product.js
+++ controllers/Product.js
@@ -2,13 +2,16 @@ const rescue = require('express-rescue');
 const Joi = require('joi');
 const service = require('../services/Product');
 
-const create = rescue (async(req, res, next) => {
+const create = rescue(async (req, res, next) => {
   const { name, quantity } = req.body;
   const FIVE = 5;
   const CREATED = 201;
   const { error } = Joi.object({
-    name: Joi.string().min(FIVE).not().empty().required(),
-    quantity: Joi.number().integer().min(1).not().empty().required(),
+    name: Joi.string().min(FIVE).not().
+    empty().
+    required(),
+    quantity: Joi.number().integer().min(1).not().empty().
+    required(),
   }).validate(req.body);
   if (error) {
     return next(error);
