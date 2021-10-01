a04acb462ea33af6016824d8fc1d493a20ede5c7
Italo <italomatos764@gmail.com>
2021-10-01 00:44:17 -0400

    Change url connection

--- models/connection.js
+++ models/connection.js
@@ -6,10 +6,10 @@ require('dotenv').config();
 // const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 
 // conexão com o banco
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // conexão para os testes
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 let db = null;
