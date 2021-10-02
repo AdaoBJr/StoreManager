233cccc10ec38a7dbb7ac4e06031e7ab6551eb1e
marcos <marcosd3wow@gmail.com>
2021-10-02 11:52:15 -0300

    req 6, teste local ok, graças a deus

--- services/salesService.js
+++ services/salesService.js
@@ -2,6 +2,13 @@ const salesModel = require('../models/salesModel');
 
 const add = async (itensSold) => salesModel.add(itensSold);
 
+const getAll = async () => salesModel.getAll()
+  .then((sales) => ({ sales }));
+
+const getById = async (id) => salesModel.getById(id); 
+
 module.exports = {
   add,
+  getAll,
+  getById,
 };
