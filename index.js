9f99c8244e716cc1558eb11c4081c2d90a482514
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-10-03 15:08:40 -0300

    Estrutura basica

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+function listeningPort() {
+  return console.log(`Aplicação ouvindo a porta ${PORT}!`)
+};
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, listeningPort);
