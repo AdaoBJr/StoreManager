d485c14b05039752c9d14aa03858721d0a6d8844
Max <gmaxgomes@gmail.com>
2021-10-04 16:28:40 -0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+
+const route = require('./Routes');
+const error = require('./middlewares/error');
+
+const PORT = 3000;
+
+app.use(bodyParser.json());
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+route.product(app);
+route.sales(app);
+
+app.use(error);
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
