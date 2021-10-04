47f28c6b5b631bb6ab159a2666a2de2dff4e34a6
Guilherme Ribeiro <guilhermedasilvar@gmail.com>
2021-10-03 22:21:34 -0300

    Requisito 02 - troca conexao

--- models/connection.js
+++ models/connection.js
@@ -6,12 +6,12 @@ const OPTIONS = {
 };
 
 // conexão do banco local
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-const DB_NAME = 'StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
 
 // conexão do avaliador
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
 
 function connection() {
   return MongoClient.connect(MONGO_DB_URL, OPTIONS)
