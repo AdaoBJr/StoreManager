9419061354e0c1155665cf6af478ad2bd3a1dd39
marcos <marcosd3wow@gmail.com>
2021-10-05 13:20:51 -0300

    finalizando os ultimos testes do modulo services

--- test/unit/services.js
+++ test/unit/services.js
@@ -91,4 +91,153 @@ describe.skip('Iniciando testes do modulo productsService', () => {
     });
   });
 
+  describe('Testando a atualização dos produtos', () => {
+    before(() => {
+      sinon.stub(productsModel, 'update').resolves({
+        name: 'Produto 1',
+        quantity: 100,
+        _id: '60e7331efa30b90f51fe8242',
+      });
+    });
+
+    after(() => {
+      productsModel.update.restore();
+    });
+
+    it('Tem que retornar o mesmo objeto recebido do model', async () => {
+      const product = await productsService.update()
+
+      expect(product).to.be.deep.equal({
+        name: 'Produto 1',
+        quantity: 100,
+        _id: '60e7331efa30b90f51fe8242',
+      })
+    });
+  });
+
+  describe('Delete dos produtos', () => {
+    before(() => {
+      sinon.stub(productsModel, 'remove').resolves({
+        name: 'Produto 1',
+        quantity: 100,
+        _id: '60e7331efa30b90f51fe8242',
+      });
+    });
+
+    after(() => {
+      productsModel.remove.restore();
+    });
+
+    it('Tem que retornar o mesmo objeto recebido do model', async () => {
+      const product = await productsService.remove()
+
+      expect(product).to.be.deep.equal({
+        name: 'Produto 1',
+        quantity: 100,
+        _id: '60e7331efa30b90f51fe8242',
+      })
+    });
+
+  });
+
+  describe('Teste do modulo salesService', () => {
+    describe('Testando a criação de vendas', () => {
+      before(() => {
+        sinon.stub(salesModel, 'add').resolves([
+          {
+            quantity: 100,
+            productId: '60e7331efa30b90f51fe8242',
+          },
+          {
+            quantity: 50,
+            productId: '60e7331efa30b90f51fe8243',
+          },
+        ]);
+      });
+  
+      after(() => {
+        salesModel.add.restore();
+      });
+  
+      it('Deve retornar um array', async () => {
+        const itensSold = await salesService.add();
+  
+        expect(itensSold).to.be.a('array');
+      });
+  
+      it('Elementos do array devm ser objetos', async () => {
+        const itensSold = await salesService.add();
+  
+        itensSold.forEach((item) => expect(item).to.be.a('object'))
+      });
+  
+      it('Elementos do array devem possuir as chaves "quantity" e "productId"', async () => {
+        const itensSold = await salesService.add();
+  
+        itensSold.forEach((item) => expect(item).to.include.all.keys('productId', 'quantity'))
+      });
+  
+    });
+  
+    describe('Testando a leitura dos produtos', () => {
+      let products;
+  
+      before(async () => {
+        products = await productsService.getAll();
+  
+        sinon.stub(productsModel, 'getAll').resolves([
+          {
+            name: 'Produto 1',
+            quantity: 100,
+            _id: '60e7331efa30b90f51fe8242',
+          },
+          {
+            name: 'Produto 2',
+            quantity: 50,
+            _id: '60e7331efa30b90f51fe8243',
+          },
+        ]);
+  
+        sinon.stub(productsModel, 'getById').resolves({
+          name: 'Produto 1',
+          quantity: 100,
+          _id: '60e7331efa30b90f51fe8242',
+        });
+      });
+  
+      after(() => {
+        productsModel.getAll.restore();
+      });
+  
+      it('Tem que retornar um objeto', async () => {
+        expect(products).to.be.a('object');
+      });
+  
+      it('Este objeto deve possuir a chave "products"', async () => {
+        expect(products).to.include.keys('products');
+      });
+  
+      it('Esta chave deve ser um array', () => {
+        expect(products.products).to.be.a('array');
+      });
+  
+      it('Elementos da chave products devem ser objetos', () => {
+        products.products.forEach((product) => expect(typeof product === 'object'));
+      });
+  
+      it('getById tem que retornar um objeto', async () => {
+        const product = await productsService.getById();
+  
+        expect(product).to.be.a('object');
+      });
+  
+      it('getById tem que possuir as chaves "name", "quantity", "_id"', async () => {
+        const product = await productsService.getById();
+  
+        expect(product).to.include.all.keys('name', 'quantity', '_id');
+      });
+    });
+  
+
+});
 });
