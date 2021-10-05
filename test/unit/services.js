8e9122b9bb3fbfde139392d06e8c65a9431137c6
marcos <marcosd3wow@gmail.com>
2021-10-05 13:12:02 -0300

    iniciando testes do modulo services

--- test/unit/services.js
+++ test/unit/services.js
@@ -1,2 +1,35 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
+const productsModel = require('../../models/productsModel');
+const productsService = require('../../services/productsService');
+const salesModel = require('../../models/salesModel');
+const salesService = require('../../services/salesService');
+
+describe.skip('Iniciando testes do modulo productsService', () => {
+  describe('Testando a criação de produtos', () => {
+    before(() => {
+      sinon.stub(productsModel, 'add').resolves((name, quantity) => ({
+        name: name,
+        quantity: quantity,
+        _id: '60e7331efa30b90f51fe8242',
+      }));
+    });
+
+    after(() => {
+      productsModel.add.restore();
+    });
+
+    it('O retorno deve ser um objeto', async () => {
+      const insertedProduct = await productsService.add();
+
+      expect(insertedProduct('Produto teste', 1000)).to.be.a('object');
+    });
+
+    it('Deve possuir as chaves "_id", "name", "quantity"', async () => {
+      const insertedProduct = await productsService.add();
+
+      expect(insertedProduct('Produto teste', 1000)).to.include.all.keys('_id', 'name', 'quantity');
+    });
+  });
+
+});
