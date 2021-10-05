75536fcb9429f30b549a8c9bb6fc1778232851b5
Paulo Freire <paulofreire.si@outlook.com>
2021-10-05 19:11:03 -0300

    I wish I had done this sooner

--- test/unit/models.js
+++ test/unit/models.js
@@ -1,6 +1,19 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const { MongoClient, ObjectId } = require('mongodb');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 const mongoDB = require('../../models/connection');
 const testProd = require('../../models/Products');
 const testSale = require('../../models/sales');
+
+const database = new MongoMemoryServer();
+
+const connect = async () {
+    const URL = await database.getUri();
+    const OPTIONS = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+
+    return MongoClient.connect(URL, OPTIONS);
+};
