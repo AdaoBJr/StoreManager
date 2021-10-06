aa7c141729a41e915acde96b7abaf375c36a29c9
unknown <malbathan.lima@enext.com.br>
2021-10-06 04:59:53 -0300

    Test's - bonus req

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -19,12 +19,18 @@ const getAll = rescue(async (_req, res, next) => {
   res.status(200).json(products);
 });
 
-const create = rescue(async (req, res, next) => {
+const create = rescue(async (req, res) => {
   const productArray = req.body;
   
   const newSale = await salesService.create(productArray);
-  if (newSale.error) return next(newSale);
-
+  if (newSale === null) {
+    return res.status(404).json({
+      err: {
+        code: 'stock_problem',
+        message: 'Such amount is not permitted to sell',
+      },
+    });
+  }  
   return res.status(200).json(newSale);
 });
 
