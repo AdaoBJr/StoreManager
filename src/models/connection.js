c81387eccb638c4e50e72152b6517f3d8d8dfcc3
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-18 15:23:20 -0300

    Update connection to use dotenv

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,15 +1,13 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const OPTIONS = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = process.env.LOCAL || 'mongodb://mongodb:27017/StoreManager';
 const DB_NAME = 'StoreManager';
-
-// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
 
 let db = null;
 
